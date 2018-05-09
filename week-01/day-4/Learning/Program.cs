ae2f1c895ffdfea31f7459b284fd7a88a446d2d0
Andras <palfi.andras89@gmail.com>
2018-05-09 21:08:08 +0200

    Added Example.ps to day 4 folder

---
+++ week-01/day-4/Learning/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //This is how I make comments.
+            string name; //variable names cant start with numbers, plus they cant contain spaces
+            int age;
+            bool isGood;
+            double x;
+            */
+            /*
+             string message = "Write your name:";
+             string yourname;
+
+             Console.Write(message);
+
+             yourname = Console.ReadLine();
+
+             Console.WriteLine("Your name is " + yourname);
+
+             Console.ReadLine();
+             */
+            /*
+           bool val1 = false;
+           bool val2 = true;
+
+           bool result1 = val1 && val2;
+           bool result2 = val1 || val2;
+           bool result3 = !(val1);
+
+           Console.WriteLine(result1);
+           Console.WriteLine(result2);
+           Console.WriteLine(result3);
+
+
+           Console.ReadLine();
+           */
+            /*
+             string mystring = "Hello";
+
+             if (mystring == "hello")
+             {
+                 Console.WriteLine("Good for you");
+             }
+             else
+             {
+                 Console.WriteLine("Not good for you");
+             }
+             Console.ReadLine();
+             */
+
+            /*
+            Console.WriteLine("please enter a number!");
+            int num = int.Parse(Console.ReadLine());
+
+            switch (num)
+            {
+                case 10:
+                    Console.WriteLine("Your number is 10");
+                    break;
+                case 20:
+                    Console.WriteLine("Your number is 20");
+                    break;
+                case 30:
+                    Console.WriteLine("Your number is 30");
+                    break;
+                default:
+                    Console.WriteLine("whatever");
+                    break;                
+            }
+
+            Console.ReadLine();
+            */
+            /*
+            while(unitstoship > 0)
+            {
+                TRuckLoads++;
+                unitstoship -= Truck_capacity;
+            }
+
+            int monthcounter = 1;
+            while(monthcounter <= monthtoinvest)
+            {
+                futurevalue = (futurevalue + monthlyinvestment) * (1 + monthlyinterestrate);
+                monthcounter++;
+            }
+
+            for(monthcounter = 1; monthcounter <= monthtoinvest; monthcounter++)
+            {
+                futurevalue = (futurevalue + monthlyinvestment) * (1 + monthlyinterestrate);
+            }
+
+            bool needinput = true;
+
+            do
+            {
+                if () { }
+                else { }
+            } while (needinput);
+            */
+
+
+
+
+        }
+    }
+}
