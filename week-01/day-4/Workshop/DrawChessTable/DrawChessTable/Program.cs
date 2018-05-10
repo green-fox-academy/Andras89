5b8bc5fd83b01563cae9a71fdcadf7022b97e699
Andras <palfi.andras89@gmail.com>
2018-05-10 17:22:27 +0200

    chore(DrawChessTable.cs): add

---
+++ week-01/day-4/Workshop/DrawChessTable/DrawChessTable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crate a program that draws a chess table like this
+            //
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            // % % % %
+            //  % % % %
+            //
+            string firstRow = "% % % % ";
+            string secondRow = " % % % %";
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(firstRow);
+                }
+                else
+                {
+                    Console.WriteLine(secondRow);
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
