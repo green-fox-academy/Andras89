dc1a85bb86e3fe2304af4b23612942d94cf7fbbd
Andras <palfi.andras89@gmail.com>
2018-05-10 17:15:16 +0200

    chore(GuessTheNumber.cs): add

--- week-01/day-4/Workshop/GuessTheNumber/GuessTheNumber/Program.cs
+++ week-01/day-4/Workshop/GuessTheNumber/GuessTheNumber/Program.cs
@@ -13,6 +13,26 @@ namespace GreenFox
             // The stored number is higher
             // The stried number is lower
             // You found the number: 8
+            int number = 8;
+            int guess;
+            do
+            {
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess < number)
+                {
+                    Console.WriteLine("The stored number is higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("The stored number is lower");
+                }
+                else
+                {
+                    Console.WriteLine("You found the number: 8");
+                }
+            } while(guess != number);
+            Console.ReadLine();
         }
     }
 }
