345a405ddfa4fd72d20edc62611df30aac92a723
Andras <palfi.andras89@gmail.com>
2018-05-11 11:07:03 +0200

    chore(PrintBigger.cs)

---
+++ week-01/day-4/Workshop/PrintBigger/PrintBigger/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+
+            if (numOne > numTwo) { Console.WriteLine(numOne); }
+            else if (numOne < numTwo) { Console.WriteLine(numTwo); }
+            else { Console.WriteLine("They are equal"); }
+
+            Console.ReadLine();
+        }
+    }
+}
