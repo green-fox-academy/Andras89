824a46eb9c9bbcbfe206b7fb6e01b8835ecd4f00
Andras <palfi.andras89@gmail.com>
2018-05-11 09:33:44 +0200

    feat(.gitignore) : Mod

---
+++ week-01/day-4/Workshop/BMI/BMI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print the Body mass index (BMI) based on these values
+            double massInKg = 81.2;
+            double heightInM = 1.78;
+
+            double bmi = 0;
+
+            bmi = massInKg / heightInM / heightInM;
+            Console.WriteLine(bmi);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-01/day-4/Workshop/Cuboid/Cuboid/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that stores 3 sides of a cuboid as variables (doubles)
+            // The program should write the surface area and volume of the cuboid like:
+            //
+            // Surface Area: 600
+            // Volume: 1000
+
+            double aSide = double.Parse(Console.ReadLine());
+            double bSide = double.Parse(Console.ReadLine());
+            double cSide = double.Parse(Console.ReadLine());
+
+            double cubeVolume = aSide * bSide * cSide;
+        }
+    }
+}
