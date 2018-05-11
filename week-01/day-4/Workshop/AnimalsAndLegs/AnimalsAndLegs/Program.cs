2f46387728ff39a488226f1f27fdda98daf27ffd
Andras <palfi.andras89@gmail.com>
2018-05-11 11:00:45 +0200

    chore(AvarageOfInput.cs)

---
+++ week-01/day-4/Workshop/AnimalsAndLegs/AnimalsAndLegs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The second represents the number of pigs owned by the farmer
+            // It should print how many legs all the animals have
+
+            int chickenNumber = int.Parse(Console.ReadLine());
+            int pigNumber = int.Parse(Console.ReadLine());
+
+            int chickenLegCount = 2;
+            int pigLegCount = 4;
+
+            int animalLegsNumber = chickenNumber * chickenLegCount + pigNumber * pigLegCount;
+
+            Console.WriteLine(animalLegsNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-01/day-4/Workshop/AvarageOfInput/AvarageOfInput/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for 5 integers in a row,
+            // then it should print the sum and the average of these numbers like:
+            //
+            // Sum: 22, Average: 4.4
+            int num = 0;
+            double sum = 0;
+            double avg = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+
+            avg = sum / 5;
+            Console.WriteLine(avg);
+
+            Console.ReadLine();
+        }
+    }
+}
