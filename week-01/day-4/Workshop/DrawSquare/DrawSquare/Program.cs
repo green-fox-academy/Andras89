3a054d496e059e193a9a49f021a48eb05773e4b4
Andras <palfi.andras89@gmail.com>
2018-05-11 13:05:01 +0200

    chore(DrawSquare.cs)

---
+++ week-01/day-4/Workshop/DrawSquare/DrawSquare/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // square like this:
+            //
+            //
+            // %%%%%
+            // %   %
+            // %   %
+            // %   %
+            // %   %
+            // %%%%%
+            //
+            // The square should have as many lines as the number was
+
+            int number = int.Parse(Console.ReadLine());
+            string temp = "";
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i == 0 || i == number-1)
+                {
+                    for (int j = 0; j < number; j++)
+                    {
+                        temp += "%";
+                    }
+                    Console.WriteLine(temp);
+                    temp = "";
+                }
+                else
+                {
+                    temp += "%";
+                    for (int j = 1; j < number-1; j++)
+                    {
+                        temp += " ";
+                    }
+                    temp += "%";
+                    Console.WriteLine(temp);
+                    temp = "";
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
