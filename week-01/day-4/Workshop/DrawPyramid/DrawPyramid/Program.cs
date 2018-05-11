6c919f6e588c033548d4fe257158349d670504a0
Andras <palfi.andras89@gmail.com>
2018-05-11 12:06:13 +0200

    chore(DrawPiramid.cs)

---
+++ week-01/day-4/Workshop/DrawPyramid/DrawPyramid/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number from the standard input, then draws a
+            // pyramid like this:
+            //
+            //
+            //    *
+            //   ***
+            //  *****
+            // *******
+            //
+            // The pyramid should have as many lines as the number was
+
+            int num, i, j, k;
+            Console.WriteLine("enter the level:");
+
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= num; i++)
+            {
+                for (j = 1; j < num - i + 1; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (k = 1; k <= i; k++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
