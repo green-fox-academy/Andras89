39cd8f6c6f9bb5aa0785cb9e0235abda67b9cb72
Andras <palfi.andras89@gmail.com>
2018-05-14 12:01:57 +0200

    chore(Factorial)

---
+++ week-02/day-01/Factorial/Factorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+
+            Console.WriteLine(factorio());
+            Console.ReadLine();
+        }
+
+        public static int factorio()
+        {
+            int factNumber = int.Parse(Console.ReadLine());
+            int factorialEquals = 1;
+            for (int i = 1; i <= factNumber; i++)
+            {
+                factorialEquals *= i;
+            }
+            return factorialEquals;
+        }
+    }
+}
