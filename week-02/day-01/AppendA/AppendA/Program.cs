49c02ae6980f046b06476bba7565fbc27ec5f584
Andras <palfi.andras89@gmail.com>
2018-05-14 11:50:38 +0200

    chore(appendA)

---
+++ week-02/day-01/AppendA/AppendA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AppendAFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `am` and assign the value `kuty` to it
+            // - Write a function called `appendA` that gets a string as an input,
+            //   appends an 'a' character to its end and returns with a string
+            // - Print the result of `appendAFunc(am)`
+            string am = "kuty";
+            Console.WriteLine(appendA(am));
+            Console.ReadLine();
+        }
+
+        public static string appendA(string word)
+        {
+            return word + "a";
+        }
+    }
+}
