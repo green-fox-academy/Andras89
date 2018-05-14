6169b0be96b723af7c5799de929af9225b4aacb6
Andras <palfi.andras89@gmail.com>
2018-05-14 12:45:39 +0200

    chore(IncrementElement)

---
+++ week-02/day-01/IncrementElement/IncrementElement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IncrementElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //- Create an array variable named `t`
+            //  with the following content: `[1, 2, 3, 4, 5]`
+            //- Increment the third element
+            //- Print the third element
+            int[] t = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(incrementElement(t));
+
+            Console.ReadLine();
+        }
+        public static int incrementElement(int[] array)
+        {
+            array[2]++;
+            return array[2];
+        }
+    }
+}
