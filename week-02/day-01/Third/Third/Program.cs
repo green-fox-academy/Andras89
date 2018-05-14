cfcd2863a1f4e71525c085711c9a920bf16c6a36
Andras <palfi.andras89@gmail.com>
2018-05-14 12:28:12 +0200

    chore(Third)

---
+++ week-02/day-01/Third/Third/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Third
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `q`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print the third element of `q`
+            int[] q = { 4, 5, 6, 7 };
+            Console.WriteLine(third(q));
+
+            Console.ReadLine();
+        }
+
+        public static int third(int[] array)
+        {
+            return array[2];
+        }
+    }
+}
