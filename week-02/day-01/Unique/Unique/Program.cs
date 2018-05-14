e4841805e3c3cc32fff75a1bb86f4d195c878ef7
Andras <palfi.andras89@gmail.com>
2018-05-14 20:30:07 +0200

    feat(Unique)

--- week-02/day-01/Unique/Unique/Program.cs
+++ week-02/day-01/Unique/Unique/Program.cs
@@ -11,15 +11,64 @@ namespace GreenFox
             //  Returns a list of numbers where every number in the list occurs only once
 
             //  Example
-            List<int> list = Unique(new int[]{ 1, 11, 34, 11, 52, 61, 1, 34});
+            int[] array = Unique(new int[] { 1, 11, 34, 11, 52, 61, 1, 34 });
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            /* List<int> list = Unique(new int[]{ 1, 11, 34, 11, 52, 61, 1, 34});
             for (int i = 0; i < list.Count; i++)
             {
                 Console.Write(list[i]+ " ");
-            }
+            }*/
             //  should print: `[1, 11, 34, 52, 61]`
             Console.ReadLine();
         }
-        public static List<int> Unique(int[] array)
+
+        public static int[] Unique(int[] array)
+        {
+            int[] tempArray = new int[array.Length];
+            bool contains;
+            int index = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                contains = true;
+                
+                for (int j = 0; j < tempArray.Length; j++)
+                {
+                    if (array[i] == tempArray[j])
+                    {
+                        contains = false;
+                    }
+                }
+                if (contains)
+                {
+                    tempArray[index] = array[i];
+                    index++;
+                }
+            }
+
+            int counter = 0;
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                if (tempArray[i] != 0)
+                {
+                    counter++;
+                }
+            }
+
+            int[] resultArray = new int[counter];
+
+            for (int i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = tempArray[i];
+            }
+
+            return resultArray;
+        }
+        /*public static List<int> Unique(int[] array)
         {
             List<int> list = new List<int>();
 
@@ -31,6 +80,6 @@ namespace GreenFox
                 }
             }
             return list;
-        }
+        }*/
     }
 }
