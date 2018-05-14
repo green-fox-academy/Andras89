0c3bb4462f2ea80e5896368177b5f02fbe67b9e3
Andras <palfi.andras89@gmail.com>
2018-05-14 13:58:54 +0200

    chore(SwapElements)

---
+++ week-02/day-01/SwapElements/SwapElements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SwapElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `abc`
+            //   with the following content: `["first", "second", "third"]`
+            // - Swap the first and the third element of `abc`
+            string[] abc = { "first", "second", "third" };
+            abc = SwapElement(abc);
+
+            for (int i = 0; i < abc.Length; i++)
+            {
+                Console.WriteLine(abc[i]);
+            }
+
+            Console.ReadLine();
+        }
+        public static string[] SwapElement(string[] array)
+        {
+            string temp = "";
+            temp = array[0];
+            array[0] = array[2];
+            array[2] = temp;
+
+            return array;
+        } 
+    }
+}
