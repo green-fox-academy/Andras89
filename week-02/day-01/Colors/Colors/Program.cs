71910c2b554e7fefb91beda0c7b541f4df4414b5
Andras <palfi.andras89@gmail.com>
2018-05-14 13:48:23 +0200

    chore(Colors)

---
+++ week-02/day-01/Colors/Colors/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a two dimensional array
+            //   which can contain the different shades of specified colors
+            // - In `colors[0]` store the shades of green:
+            //   `"lime", "forest green", "olive", "pale green", "spring green"`
+            // - In `colors[1]` store the shades of red:
+            //   `"orange red", "red", "tomato"`
+            // - In `colors[2]` store the shades of pink:
+            //   `"orchid", "violet", "pink", "hot pink"`
+            string[][] array = new string[][]
+            {
+                new string[] {"lime", "forest green", "olive", "pale green", "spring green"},
+                new string[] {"orange red", "red", "tomato"},
+                new string[] {"orchid", "violet", "pink", "hot pink"}
+            };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    Console.Write(array[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
