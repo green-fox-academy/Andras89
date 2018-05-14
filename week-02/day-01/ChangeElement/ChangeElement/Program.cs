ed1bd861bcd2319c176a916ac3e50d76cfc5029e
Andras <palfi.andras89@gmail.com>
2018-05-14 12:41:47 +0200

    chore(changeElement)

---
+++ week-02/day-01/ChangeElement/ChangeElement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChangeElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `s`
+            //   with the following content: `[1, 2, 3, 8, 5, 6]`
+            // - Change the 8 to 4
+            // - Print the fourth element
+            int[] s = { 1, 2, 3, 8, 5, 6 };
+            Console.WriteLine(changeElement(s));
+            Console.ReadLine();
+        }
+        public static int changeElement(int[] array)
+        {
+            array[3] = 4;
+            return array[3];
+        }
+    }
+}
