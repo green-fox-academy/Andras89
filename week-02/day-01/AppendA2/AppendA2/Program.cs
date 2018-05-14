9f13ca3269669c44aeeea9d98822a25dc77efc35
Andras <palfi.andras89@gmail.com>
2018-05-14 13:52:38 +0200

    chore(AppendA2)

---
+++ week-02/day-01/AppendA2/AppendA2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AppendA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `animals`
+            //   with the following content: `["kuty", "macsk", "cic"]`
+            // - Add all elements an `"a"` at the end
+            string[] animals = { "kuty", "macsk", "cic" };
+            animals = AppenA(animals);
+
+            for (int i = 0; i < animals.Length; i++)
+            {
+                Console.WriteLine(animals[i]);
+            }
+            Console.ReadLine();
+        }
+        public static string[] AppenA(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += "a";
+            }
+            return array;
+        }
+    }
+}
