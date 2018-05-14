3a91e77283dfca1ab7f0115a197e8b14e1d999b4
Andras <palfi.andras89@gmail.com>
2018-05-14 11:46:20 +0200

    chore(Greet)

---
+++ week-02/day-01/Greet/Greet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Greet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create a string variable named `al` and assign the value `Greenfox` to it
+            // - Create a function called `greet` that greets it's input parameter
+            //     - Greeting is printing e.g. `Greetings dear, Greenfox`
+            // - Greet `al`
+            string al = "Greenfox";
+            greet(al);
+            Console.ReadLine();
+        }
+
+        public static void greet(string name)
+        {
+            Console.WriteLine("Greetings dear, {0}", name);
+        }
+    }
+}
