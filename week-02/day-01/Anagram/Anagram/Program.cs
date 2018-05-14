53a90249693f2a1d29bb03bb7ea6218f0d696bd9
Andras <palfi.andras89@gmail.com>
2018-05-14 15:49:19 +0200

    chore(Anagram)

---
+++ week-02/day-01/Anagram/Anagram/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = Console.ReadLine();
+            string secondWord = Console.ReadLine();
+
+            string[] arrayWords = new string[] { firstWord, secondWord };
+            Console.WriteLine(IsAnagram(arrayWords));
+            Console.ReadLine();
+        }
+        public static bool IsAnagram(string[] array)
+        {
+            bool isAnag;
+            string tempWord = "";
+            for (int i = array[0].Length-1; i >=0 ; i--)
+            {
+                tempWord += array[0][i];
+            }
+            isAnag = (tempWord.Equals(array[1]));
+            return isAnag;
+
+        }
+    }
+}
