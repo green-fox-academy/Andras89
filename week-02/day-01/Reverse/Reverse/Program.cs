7549ab0c7d78db31b519da9350246f4f82d8f13a
Andras <palfi.andras89@gmail.com>
2018-05-14 14:13:13 +0200

    chore(Reverse)

---
+++ week-02/day-01/Reverse/Reverse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `aj`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Reverse the order of the elements in `aj`
+            // - Print the elements of the reversed `aj`
+            int[] aj = { 3, 4, 5, 6, 7 };
+            aj = ReverseArray(aj);
+
+            for (int i = 0; i < aj.Length; i++)
+            {
+                Console.WriteLine(aj[i]);
+            }
+
+            Console.ReadLine();
+        }
+        public static int[] ReverseArray(int[] array)
+        {
+            int[] newArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[array.Length - (i + 1)] = array[i];
+            }
+            return newArray;
+        }
+    }
+}
