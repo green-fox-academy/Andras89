9c3816c93fdb3a2c787104f25c7ea5557fdb933b
Andras <palfi.andras89@gmail.com>
2018-05-14 12:56:13 +0200

    chore(Matrix)

---
+++ week-02/day-01/Matrix/Matrix/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DiagonalMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create (dynamically) a two dimensional array
+            //   with the following matrix. Use a loop!
+            //
+            //   1 0 0 0
+            //   0 1 0 0
+            //   0 0 1 0
+            //   0 0 0 1
+            //
+            // - Print this two dimensional array to the output
+            matrix();
+
+            Console.ReadLine();
+        }
+        public static void matrix()
+        {
+            int[,] mat = new int[4, 4];
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write("1 ");
+                    }
+                    else
+                    {
+                        Console.Write("0 ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
