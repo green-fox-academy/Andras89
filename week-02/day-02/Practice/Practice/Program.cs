d4d49ca4935eeb72349c2aede03073a02e10f809
Andras <palfi.andras89@gmail.com>
2018-05-14 19:16:15 +0200

    chore(practice)

---
+++ week-02/day-02/Practice/Practice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sampleString1 = "ThisIsaString";
+
+            Console.WriteLine(SubStringGenerator(sampleString1));
+
+            Console.ReadLine();
+        }
+
+        public static string SubStringGenerator(string word)
+        {
+            word = word.Substring(1, 4);
+            return word;
+        }
+    }
+}
