f95574dffdce35103bbcf93d0f2f356ada06dd3b
Andras <palfi.andras89@gmail.com>
2018-05-14 12:48:56 +0200

    chore(PrintElements)

---
+++ week-02/day-01/PrintElement/PrintElement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrintAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `numbers`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print all the elements of `numbers`
+            int[] numbers = { 4, 5, 6, 7 };
+            printElements(numbers);
+
+            Console.ReadLine();
+        }
+        public static void printElements(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
