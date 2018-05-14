6ec02f9c5f65128fbd1e3d0baf1e10fc70bf8538
Andras <palfi.andras89@gmail.com>
2018-05-14 11:41:35 +0200

    chore(Doubling)

---
+++ week-01/day-4/Workshop/ParametricAvarage/ParametricAvarage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for a number.
+            // It would ask this many times to enter an integer,
+            // if all the integers are entered, it should print the sum and average of these
+            // integers like:
+            //
+            // Sum: 22, Average: 4.4
+
+            int numberOfNumbers = int.Parse(Console.ReadLine());
+            int sum = 0;
+            double avg = 0;
+
+            for (int i = 0; i < numberOfNumbers; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            avg = sum / numberOfNumbers;
+
+            Console.WriteLine(avg);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week-02/day-01/Doubling/Doubling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Doubling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an integer variable named `baseNum` and assign the value `123` to it
+            // - Create a function called `doubling` that doubles it's input parameter and returns with an integer
+            // - Print the result of `doubling(baseNum)`
+            int baseNum = 123;
+            Console.WriteLine(Doubling(baseNum));
+
+            Console.ReadLine();
+        }
+
+        public static int Doubling(int num)
+        {
+            return num *= 2;
+        } 
+    }
+}
