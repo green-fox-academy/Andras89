c9ea636e9b090ad04114dc492a6cce24b85d22ae
Andras <palfi.andras89@gmail.com>
2018-05-14 12:38:20 +0200

    chore(SumElements)

---
+++ week-02/day-01/SumElements/SumElements/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `r`
+            //   with the following content: `[54, 23, 66, 12]`
+            // - Print the sum of the second and the third element
+            int[] r = {54, 23, 66, 12 };
+            Console.WriteLine(sumElements(r));
+
+            Console.ReadLine();
+        }
+        public static int sumElements(int[] array)
+        {
+            return array[1] + array[2];
+        }
+    }
+}
