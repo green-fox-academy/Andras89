8c699b1a9bba33cdb109d9c2c036f2270ac397fb
Andras <palfi.andras89@gmail.com>
2018-05-14 11:56:44 +0200

    chore(sum)

---
+++ week-02/day-01/Summing/Summing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter and returns with an integer
+            Console.WriteLine(sum());
+            Console.ReadLine();
+        }
+
+        public static int sum()
+        {
+            int sum = 0;
+            int numberOfSummedNumbers = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberOfSummedNumbers; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            return sum;
+        }
+    }
+}
