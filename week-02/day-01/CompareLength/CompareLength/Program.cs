8e445c68e492fdfbd097f8c3b3940c1d100c4f82
Andras <palfi.andras89@gmail.com>
2018-05-14 12:35:27 +0200

    chore(CompareLength)

---
+++ week-02/day-01/CompareLength/CompareLength/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CompareLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `p1`
+            //   with the following content: `[1, 2, 3]`
+            // - Create an array variable named `p2`
+            //   with the following content: `[4, 5]`
+            // - Print if `p2` has more elements than `p1`
+            int[] p1 = {1, 2, 3};
+            int[] p2 = {4, 5};
+
+            Console.WriteLine(compareLength(p1, p2));
+
+            Console.ReadLine();
+        }
+        public static bool compareLength(int[] p1, int[] p2)
+        {
+            return p1.Length < p2.Length;
+        }
+    }
+}
