57f2d75f8f263d73f341a7a3b6ea7cbfea6e858c
Andras <palfi.andras89@gmail.com>
2018-05-14 21:31:03 +0200

    feat(PalindromeSearch)

--- week-02/day-01/PalindromeSearcher/PalindromeSearcher/Program.cs
+++ week-02/day-01/PalindromeSearcher/PalindromeSearcher/Program.cs
@@ -10,12 +10,52 @@ namespace PalindromeSearcher
     {
         static void Main(string[] args)
         {
-            string word = Console.ReadLine();
+            string word = "dog goat dad duck doodle never";
+            string[] palindromeArray = SearchPalindrome(word);
+
+            for (int i = 0; i < palindromeArray.Length; i++)
+            {
+                Console.Write(palindromeArray[i] + ", ");
+            }
+
+            Console.ReadLine();
         }
-    }
 
-    public static string[] SearchPalindrome(string word)
-    {
+        public static string[] SearchPalindrome(string word)
+        {
+            string[] palindromeArray = new string[7];
+            int index = 0;
 
+            for (int i = 0; i < word.Length; i++)
+            {
+                for (int j = 1; j < word.Length - i; j++)
+                {
+                    if (IsPalindrome(word.Substring(i, j)) && word.Substring(i, j).Length >= 3)
+                    {
+                        palindromeArray[index] = word.Substring(i, j);
+                        index++;
+                    }
+                }
+            }
+
+            return palindromeArray;
+        }
+
+        public static bool IsPalindrome(string word)
+        {
+            bool palindrome = false;
+            string tempWord = string.Empty;
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                tempWord += word[i];  
+            }
+
+            if (tempWord.Equals(word, StringComparison.CurrentCultureIgnoreCase))
+            {
+                palindrome = true;
+            }
+            return palindrome;
+        }
     }
 }
