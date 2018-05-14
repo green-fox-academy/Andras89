2b09ba2f526e6100034b3490a5ffccf933e0e052
Andras <palfi.andras89@gmail.com>
2018-05-14 15:53:25 +0200

    chore(Palindrome)

---
+++ week-02/day-01/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = Console.ReadLine();
+            Console.WriteLine(CreatePalindrome(firstWord));
+
+            Console.ReadLine();
+        }
+        public static string CreatePalindrome(string word)
+        {
+            string tempWord = word;
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                tempWord += word[i];
+            }
+            return tempWord;
+        }
+    }
+}
