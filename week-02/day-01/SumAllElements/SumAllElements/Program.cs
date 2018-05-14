a22ef7464464c947cbca4e351e989974e52b877f
Andras <palfi.andras89@gmail.com>
2018-05-14 14:07:34 +0200

    chore(SumAllElements)

---
+++ week-02/day-01/SumAllElements/SumAllElements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Create an array variable named `ai`
+            //   with the following content: `[3, 4, 5, 6, 7]`
+            // - Print the sum of the elements in `ai`
+            int[] ai = { 3, 4, 5, 6, 7 };
+            Console.WriteLine(SumOfElements(ai));
+
+            Console.ReadLine();
+        }
+        public static int SumOfElements(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+    }
+}
