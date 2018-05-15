952f84a4d89c0f5989383e13f8c34d3629022e01
Andras <palfi.andras89@gmail.com>
2018-05-15 13:31:18 +0200

    add(List-02)

---
+++ week-02/day-02/List-02/List-02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matchmaking
+{
+    public class Matchmaking
+    {
+        public static void Main(string[] args)
+        {
+            var girls = new List<string> { "Eve", "Ashley", "Bözsi", "Kat", "Jane" };
+            var boys = new List<string> { "Joe", "Fred", "Béla", "Todd", "Neef", "Jeff" };
+            Dictionary<string, string> allThePairs = new Dictionary<string, string>();
+
+            allThePairs = MatchmakingMethod(girls, boys);
+
+            foreach (KeyValuePair<string, string> member in allThePairs)
+            {
+                Console.WriteLine(member.Key + " : " + member.Value);
+            }
+            // Write a method that joins the two lists by matching one girl with one boy into a new list
+            // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+
+            Console.ReadLine();
+        }
+
+        public static Dictionary<string, string> MatchmakingMethod(List<string> girls, List<string> boys)
+        {
+            Dictionary<string, string> inputThePeople = new Dictionary<string, string>();
+            for (int i = 0; i < girls.Count; i++)
+            {
+                inputThePeople[girls[i]] = boys[i];
+            }
+
+            return inputThePeople;
+        }
+    }
+}
