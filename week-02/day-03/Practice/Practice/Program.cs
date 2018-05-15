f707b38969f3eda0e8ef9980777faddc74b087a4
Andras <palfi.andras89@gmail.com>
2018-05-15 19:45:10 +0200

    add(day-03): Practice

---
+++ week-02/day-03/Practice/Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Filename = @"C:\Green Fox\Andras89\week-02\day-03\Practice\mydoc.txt";
+
+            WriteFile(Filename);
+
+
+            ReadFile(Filename);
+        }
+
+        public static void WriteFile(string Filename)
+        {
+            File.WriteAllText(Filename, "Helo World!");
+        }
+        public static void ReadFile(string Filename)
+        {
+            Console.WriteLine(File.ReadAllText(Filename)); 
+        }
+    }
+}
