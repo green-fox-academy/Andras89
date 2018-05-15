e6691afb7a636a0691d754dc0108af9510787af7
Andras <palfi.andras89@gmail.com>
2018-05-15 11:41:53 +0200

    add(string-01)

---
+++ week-02/day-02/String-01/String-01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleReplace
+{
+    public class SimpleReplace
+    {
+        public static void Main(string[] args)
+        {
+            string example = "In a dishwasher far far away";
+
+            example.Replace("dishwasher", "galaxy");
+
+            // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+            // Please fix it for me!
+            // Expected ouput: In a galaxy far far away
+
+            Console.WriteLine(example);
+        }
+    }
+}
