d57836ef2724eae2bb21c3dfb6dc65454844ece6
Andras <palfi.andras89@gmail.com>
2018-05-15 12:40:25 +0200

    add(string-05)

---
+++ week-02/day-02/String-05/String-05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Reverse
+{
+    public class Reverse
+    {
+        public static void Main(string[] args)
+        {
+            string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a method that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+
+            Console.WriteLine(ReversedMethod(reversed));
+
+            Console.ReadLine();
+        }
+
+        public static string ReversedMethod(string sentence)
+        {
+            string returnSentence = String.Empty;
+            for (int i = sentence.Length - 1; i >= 0; i--)
+            {
+                returnSentence += sentence[i];
+            }
+
+            return returnSentence;
+        }
+    }
+}
