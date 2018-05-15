bf14db9870c4ca17e4495cec1c2fb8a6631479a3
Andras <palfi.andras89@gmail.com>
2018-05-15 16:01:46 +0200

    add(Dictionary-01)

---
+++ week-02/day-02/Dictionary-01/Dictionary-01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HeWillNever
+{
+    public class HeWillNever
+    {
+        public static void Main(string[] args)
+        {
+            string output = "";
+            int[] notSoCrypticMessage = { 1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11 };
+
+            var map = new Dictionary<int, string>();
+
+            map.Add(7, "run around and desert you");
+            map.Add(50, "tell a lie and hurt you ");
+            map.Add(49, "make you cry, ");
+            map.Add(2, "let you down");
+            map.Add(12, "give you up, ");
+            map.Add(1, "Never gonna ");
+            map.Add(11, "\n");
+            map.Add(3, "say goodbye ");
+
+            foreach (int item in notSoCrypticMessage)
+            {
+                output += map[item] + "\n";
+            }
+
+            // Things are a little bit messed up
+            // Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
+            // Assemble the fragments into the out variable
+
+            Console.WriteLine(output);
+
+            Console.ReadLine();
+        }
+    }
+}
