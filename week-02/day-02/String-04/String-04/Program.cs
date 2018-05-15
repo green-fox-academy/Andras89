05d43060b020bd4d12888274e39d4b32828ee266
Andras <palfi.andras89@gmail.com>
2018-05-15 12:25:26 +0200

    add(string-04)

---
+++ week-02/day-02/String-04/String-04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace TodoPrint
+{
+    public class TodoPrint
+    {
+        public static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the todoText
+            // Add " - Download games" to the end of the todoText
+            // Add " - Diablo" to the end of the todoText but with indentation
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            StringBuilder todoTextRedone = new StringBuilder(todoText);
+            todoTextRedone.Insert(0, "My todo:\n\t")
+                          .Append("\t - Download games\n\t\t - Diablo");
+
+            todoText = todoTextRedone.ToString();
+
+            Console.WriteLine(todoText);
+
+            Console.ReadLine();
+        }
+    }
+}
