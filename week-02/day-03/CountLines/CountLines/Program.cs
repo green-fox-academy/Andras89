0632614b9f340c032cef1e3e5d69cd60dde80f8d
Andras <palfi.andras89@gmail.com>
2018-05-16 13:02:39 +0200

    add(CountLines)

---
+++ week-02/day-03/CountLines/CountLines/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CountLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a function that takes a filename as string,
+            // then returns the number of lines the file contains.
+            // It should return zero if it can't open the file, and
+            // should not raise any error.
+            string fileName = "my-file.txt";
+
+            Console.WriteLine(CountLine(fileName)); 
+
+            Console.ReadLine();
+        }
+        public static int CountLine(string fileName)
+        {
+            int counter = 0;
+
+            try
+            {
+                StreamReader sr = new StreamReader(fileName);
+                string getLine = sr.ReadLine();
+                counter++;
+                while (getLine != null)
+                {
+                    if (getLine != null)
+                    {
+                        counter++;
+                    }
+                    getLine = sr.ReadLine();
+                }
+                sr.Dispose();
+
+                return counter;
+            }
+            catch (Exception)
+            {
+                return counter;
+            }
+        }
+    }
+}
---
+++ week-02/day-03/CountLines/CountLines/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CountLines")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CountLines")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3cf145b8-18b7-4716-9075-3dae630956e7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
