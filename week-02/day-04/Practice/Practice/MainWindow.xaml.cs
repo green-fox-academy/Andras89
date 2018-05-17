573e183b7728daade024103c31af9bb12f1c71bc
Andras <palfi.andras89@gmail.com>
2018-05-17 12:28:16 +0200

    feat(): DrawDiagonal function

--- week-02/day-04/Practice/Practice/MainWindow.xaml.cs
+++ week-02/day-04/Practice/Practice/MainWindow.xaml.cs
@@ -16,7 +16,19 @@ namespace Drawing
             var foxDraw = new FoxDraw(canvas);
 
             //DrawPlus(canvas, foxDraw);
-            DrawDifferentSideColorBox(canvas, foxDraw);
+            //DrawDifferentSideColorBox(canvas, foxDraw);
+            DrawDiagonal(canvas, foxDraw);
+        }
+
+        public void DrawDiagonal(Canvas canvas, FoxDraw foxDraw)
+        {
+            // Draw the canvas' diagonals.
+            // If it starts from the upper-left corner it should be green, otherwise it should be red.
+            foxDraw.StrokeColor(Colors.Green);
+            foxDraw.DrawLine(0, 0, canvas.Width, canvas.Height, 10);
+            foxDraw.StrokeColor(Colors.Red);
+            foxDraw.DrawLine(canvas.Width, 0, 0, canvas.Height, 10);
+
         }
 
         public void DrawPlus(Canvas canvas, FoxDraw foxDraw)
@@ -45,5 +57,6 @@ namespace Drawing
                              canvas.Height / 4 * 3, canvas.Height / 4 * 3, 10);
 
         }
+
     }
 }
