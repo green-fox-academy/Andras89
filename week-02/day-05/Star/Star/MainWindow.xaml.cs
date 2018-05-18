4db3e5753eb4dd6b6da6fba2b4b694cb0d28a0f8
Andras <palfi.andras89@gmail.com>
2018-05-18 11:17:28 +0200

    chore(Star)

---
+++ week-02/day-05/Star/Star/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using GreenFox;
+
+namespace Drawing
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            Star(foxDraw);
+        }
+
+        private void Star(FoxDraw foxDraw)
+        {
+            foxDraw.StrokeColor(Colors.Green);
+
+            double center = canvas.Width / 2;
+
+            for (int i = 0; i < center; i += 25)
+            {
+                foxDraw.DrawLine(center, i , center+i, center, 3);
+            }
+            for (int i = 0; i < center; i += 25)
+            {
+                foxDraw.DrawLine(i, center, center, center-i-25, 3);
+            }
+
+
+            for (int i = 0; i < center; i+=25)
+            {
+                foxDraw.DrawLine(center, (center*2)-i, center - i, center, 3);
+            }
+
+            for (int i = 0; i < center; i += 25)
+            {
+                foxDraw.DrawLine(center, (center*2)-i, center + i, center, 3);
+            }
+        }
+    }
+}
