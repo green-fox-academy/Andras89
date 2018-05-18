f0c5153f0160cb9755b609fa30e6088b0f7b310b
Andras <palfi.andras89@gmail.com>
2018-05-18 10:15:14 +0200

    chore(Optical)

---
+++ week-02/day-05/OpticalLines/OpticalLines/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using GreenFox;
+
+namespace Drawing
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+            Optical(foxDraw);
+        }
+
+        public void Optical(FoxDraw foxDraw)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                foxDraw.DrawLine(0, 0, canvas.Width, (canvas.Height/10)*(i+10), 6);
+
+            }
+
+        }
+    }
+}
