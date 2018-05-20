11bf725185f3941378989edb7469b4cc6b1eef1f
Andras <palfi.andras89@gmail.com>
2018-05-20 13:09:37 +0200

    feat(SuperHexagon): IterateHexagon()

--- week-02/day-05/SuperHexagon/SuperHexagon/MainWindow.xaml.cs
+++ week-02/day-05/SuperHexagon/SuperHexagon/MainWindow.xaml.cs
@@ -15,31 +15,51 @@ namespace Drawing
         {
             InitializeComponent();
 
-            double hexaHeight = 100;
+            double hexaWholeHeight = 100;
 
-            IterateHexagon(hexaHeight);
+            IterateHexagon(hexaWholeHeight);
         }
 
-        private void IterateHexagon(double hexaHeight)
+        private void IterateHexagon(double hexaWholeHeight)
         {
-            Point hexaCenterCoords = new Point(300, 300);
+            Point hexaCenterCoords = new Point(0,0);
             List<object> hexaProperties = new List<object>();
-            
+
             hexaProperties.Add(hexaCenterCoords);
-            hexaProperties.Add(hexaHeight);
 
-            DrawHexagon(hexaProperties);
+            double yIteration = hexaWholeHeight / 2;
+            double hexaASide = Math.Sqrt(4 / 3 * yIteration * yIteration);
 
+            double hexaRadius = 2 * hexaASide / Math.Sqrt(3);
+            double xIteration = 2 * hexaRadius + hexaASide;
+
+            hexaProperties.Add(yIteration);
+            hexaProperties.Add(hexaASide);
+            hexaProperties.Add(hexaRadius);
+
+            for (double yCoord = canvas.Height-yIteration; yCoord >= yIteration; yCoord-=yIteration)
+            {
+
+                for (double xCoord = hexaRadius; xCoord <= canvas.Width - xIteration;
+                     xCoord += (2*hexaRadius + hexaASide))
+                {
+                    hexaProperties[0] = new Point(xCoord, yCoord);
+                    DrawHexagon(hexaProperties);
+                }
+
+            }
         }
 
-        private void DrawHexagon(List<object> hexagonCoordAndHeight)
+        private void DrawHexagon(List<object> hexaProperties)
         {
             foxDraw = new FoxDraw(canvas);
-            double hexaHeight = (double)hexagonCoordAndHeight[1] / 2;
-            Point centerCoords = (Point)hexagonCoordAndHeight[0];
 
-            double aSide = Math.Sqrt(4/3 * hexaHeight * hexaHeight);
-            double radius = 2 * aSide / Math.Sqrt(3);
+            Point centerCoords = (Point)hexaProperties[0];
+            double hexaHeight = (double)hexaProperties[1];
+
+            double aSide = (double)hexaProperties[2];
+            double radius = (double)hexaProperties[3];
+
             List<Point> polygonPoints = new List<Point>();
 
             foxDraw.StrokeColor(Colors.Purple);
