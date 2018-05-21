d0026d9cd15de1cd8e1b4038f76dbb55c6d3acf1
Andras <palfi.andras89@gmail.com>
2018-05-21 13:51:33 +0200

    feat(TrianglePyramidVol2): IterateTrianglePyramid()

--- week-02/day-05/TrianglePyramidVol2/TrianglePyramidVol2/MainWindow.xaml.cs
+++ week-02/day-05/TrianglePyramidVol2/TrianglePyramidVol2/MainWindow.xaml.cs
@@ -9,10 +9,41 @@ namespace Drawing
 {
     public partial class MainWindow : Window
     {
+        FoxDraw foxDraw;
+
         public MainWindow()
         {
             InitializeComponent();
-            var foxDraw = new FoxDraw(canvas);
+            double triangleHeight = 30;
+
+            IterateTrianglePyramid(triangleHeight);
+        }
+
+        public void IterateTrianglePyramid(double triangleHeight)
+        {
+            foxDraw = new FoxDraw(canvas);
+
+            double aSide = Math.Sqrt(4 / 3 * triangleHeight * triangleHeight);
+            double aSideDivTwo = aSide / 2;
+
+            double aSideDivTwoShaper = 0;
+            double aSideShaper = 0;
+
+            foxDraw.StrokeColor(Color.FromRgb(160,20,100));
+
+            for (double i = canvas.Height; i >= 0; i -= triangleHeight)
+            {
+                foxDraw.DrawLine(0+aSideDivTwoShaper, i, canvas.Width-aSideDivTwoShaper, i, 2);
+
+                foxDraw.DrawLine(0 + aSideShaper, canvas.Height,
+                                 canvas.Width/2 + aSideDivTwoShaper, canvas.Height - i, 2);
+
+                foxDraw.DrawLine(canvas.Width / 2 - aSideDivTwoShaper, canvas.Height - i,
+                                 canvas.Width - aSideShaper, canvas.Height, 2);
+
+                aSideDivTwoShaper += aSideDivTwo;
+                aSideShaper += aSide;
+            }
 
         }
     }
