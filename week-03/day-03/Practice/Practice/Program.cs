d38e0ad91e17d1bf037041cd43103710051561a3
Andras <palfi.andras89@gmail.com>
2018-05-22 20:47:55 +0200

    feat(Practice): Tree Class

--- week-03/day-03/Practice/Practice/Program.cs
+++ week-03/day-03/Practice/Practice/Program.cs
@@ -10,6 +10,21 @@ namespace Practice
     {
         static void Main(string[] args)
         {
+            Tree firstTree = new Tree();
+            Tree secondTree = new Tree();
+            Tree thirdTree = new Tree();
+            Tree fourthTree = new Tree();
+            Tree fifthTree = new Tree();
+
+            firstTree.Type = "Birch";
+            firstTree.Age = 10;
+
+            firstTree.MultiplyAgeWithAHundred();
+
+            Console.WriteLine(firstTree.Type);
+            Console.WriteLine(firstTree.Age);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ week-03/day-03/Practice/Practice/Tree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Tree
+    {
+        public string Type { get; set; }
+        public string LeafColor { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+
+        public int MultiplyAgeWithAHundred()
+        {
+            Age = Age * 100;
+            return Age;
+        }
+    }
+}
