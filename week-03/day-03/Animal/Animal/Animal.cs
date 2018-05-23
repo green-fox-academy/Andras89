742f1d77eb3bf04e84c2b37a4148664538690ff3
Andras <palfi.andras89@gmail.com>
2018-05-23 15:20:51 +0200

    refactor(Animal): deleted comments

--- week-03/day-03/Animal/Animal/Animal.cs
+++ week-03/day-03/Animal/Animal/Animal.cs
@@ -8,14 +8,6 @@ namespace Animal
 {
     class Animal
     {
-        /*
-         Every animal has a hunger value, which is a whole number
-        Every animal has a thirst value, which is a whole number
-        when creating a new animal object these values are created with the default 50 value
-        Every animal can eat() which decreases their hunger by one
-        Every animal can drink() which decreases their thirst by one
-        Every animal can play() which increases both by one*/
-
         private int Hunger;
         private int Thirst;
 
