cc5b86f110c6b27d7c65bcdb7255785d3061743c
Andras <palfi.andras89@gmail.com>
2018-05-23 20:38:54 +0200

    feat(Blog): BlogInitializer()

--- week-03/day-03/Blog/Blog/Program.cs
+++ week-03/day-03/Blog/Blog/Program.cs
@@ -10,6 +10,21 @@ namespace Blog
     {
         static void Main(string[] args)
         {
+            Blog blogList = new Blog();
+
+            blogList = BlogInitializer(blogList);
+
+
+        }
+
+        private static Blog BlogInitializer(Blog blogList)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                blogList.Add(new BlogPost($"{i+1}. John Doe", "Disturbed", "Cool Band!", "2018.05.23."));
+            }
+
+            return blogList;
         }
     }
 }
