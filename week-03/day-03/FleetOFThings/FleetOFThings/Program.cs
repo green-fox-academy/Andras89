1ca03e0cc1e97b9f6f193e02cba5ea3a954bd5d3
Andras <palfi.andras89@gmail.com>
2018-05-23 14:35:00 +0200

    feat(FleetOfThings): FleetOThings()

--- week-03/day-03/FleetOFThings/FleetOFThings/Program.cs
+++ week-03/day-03/FleetOFThings/FleetOFThings/Program.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FleetOFThings
+namespace FleetOfThings
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var fleet = new Fleet();
+            // Create a fleet of things to have this output:
+            // 1. [ ] Get milk
+            // 2. [ ] Remove the obstacles
+            // 3. [x] Stand up
+            // 4. [x] Eat lunch
+            // Hint: You have to create a Print method also
+            fleet = FleetOThings(fleet);
+        }
+
+        
+
+        public static Fleet FleetOThings(Fleet fleet)
+        {
+            Thing thingOne = new Thing("1. [ ] Get milk");
+            Thing thingTwo = new Thing("2. [ ] Remove the obstacles");
+            Thing thingThree = new Thing("3. [ ] Stand up");
+            Thing thingFour = new Thing("4. [ ] Eat lunch");
+
+            thingThree.Complete();
+            thingFour.Complete();
+
+            fleet.Add(thingOne);
+            fleet.Add(thingTwo);
+            fleet.Add(thingThree);
+            fleet.Add(thingFour);
+
+            return fleet;
         }
     }
 }
---
+++ week-03/day-03/FleetOFThings/FleetOFThings/Things.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetOfThings
+{
+    public class Thing
+    {
+        private string Name;
+        private bool Completed;
+
+        public Thing(string name)
+        {
+            this.Name = name;
+        }
+        
+        public void Complete()
+        {
+            this.Completed = true;
+        }
+
+        public string RecallName()
+        {
+            return Name;
+        }
+
+        public bool IsComplete()
+        {
+            return Completed;
+        }
+    }
+}
