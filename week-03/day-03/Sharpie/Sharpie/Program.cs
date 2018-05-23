3f132cd32ce3a4d521d03d1068ba6958acbc7cac
Andras <palfi.andras89@gmail.com>
2018-05-23 15:28:43 +0200

    feat(Sharpie): add tests

--- week-03/day-03/Sharpie/Sharpie/Program.cs
+++ week-03/day-03/Sharpie/Sharpie/Program.cs
@@ -10,6 +10,15 @@ namespace Sharpie
     {
         static void Main(string[] args)
         {
+            Sharpie numberOne = new Sharpie("Blue", 200);
+
+            for (int i = 0; i < 30; i++)
+            {
+                numberOne.Use();
+            }
+
+            Console.WriteLine(numberOne.NumberOfUsesLeft());
+            Console.ReadLine();
         }
     }
 }
