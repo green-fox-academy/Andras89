00777b571c5350cae22a8988886efce02383b374
Andras <palfi.andras89@gmail.com>
2018-05-23 10:46:30 +0200

    chore(PostIt)

---
+++ week-03/day-03/PostIt/PostIt/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Postit postitOne = new Postit("Orange", "Blue", "Idea1");
+            Postit postitTwo = new Postit("Pink", "Black", "Awesome");
+            Postit postitThree = new Postit("Yellow", "Green", "Superb");
+
+            Console.WriteLine($"{postitOne.BackgroundColor} {postitOne.TextColor} {postitOne.Text}");
+
+            Console.ReadLine();
+        }
+    }
+    struct Postit
+    {
+        public string BackgroundColor;
+        public string TextColor;
+        public string Text;
+
+        public Postit(string backgroundColor, string textColor, string text)
+        {
+            BackgroundColor = backgroundColor;
+            TextColor = textColor;
+            Text = text;
+        }
+    } 
+}
---
+++ week-03/day-03/PostIt/PostIt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PostIt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PostIt")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a858f3b9-4851-4d3e-945f-21b5d69301a4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
