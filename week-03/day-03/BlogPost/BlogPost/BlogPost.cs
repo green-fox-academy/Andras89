292db5e2115479deed666231022ea7592ab48f6d
Andras <palfi.andras89@gmail.com>
2018-05-23 10:57:26 +0200

    chore(BlogPost)

---
+++ week-03/day-03/BlogPost/BlogPost/BlogPost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPost
+{
+    class BlogPost
+    {
+        public string AuthorName;
+        public string Title;
+        public string Text;
+        public string PublicationDate;
+
+        public BlogPost(string AuthorName, string Title, string Text, string PublicationDate)
+        {
+            this.AuthorName = AuthorName;
+            this.Title = Title;
+            this.Text = Text;
+            this.PublicationDate = PublicationDate;
+        }
+    }
+}
