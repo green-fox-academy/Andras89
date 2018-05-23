f395a3831831b60dcf684b02c94e196d5741401b
Andras <palfi.andras89@gmail.com>
2018-05-23 15:56:37 +0200

    (TeachersAndStudents): Teacher class, Student class

---
+++ week-03/day-03/TeachersAndStudents/TeachersAndStudents/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachersAndStudents
+{
+    class Student
+    {
+        private string Name;
+
+        public Student()
+        {
+            
+        }
+
+        public void Learn()
+        {
+            Teacher teacher = new Teacher();
+            teacher.Teach();
+        }
+
+        public void Question()
+        {
+            Console.WriteLine("Is this a question?");
+        }
+    }
+}
---
+++ week-03/day-03/TeachersAndStudents/TeachersAndStudents/Teacher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeachersAndStudents
+{
+    class Teacher
+    {
+        private string Name;
+
+        public Teacher()
+        {
+            
+        }
+
+        public void Teach()
+        {
+            Student student = new Student();
+            student.Learn();
+        }
+
+        public void Answer()
+        {
+            Console.WriteLine("This is the answer!");
+        }
+    }
+}
