04fe81cc47eb410eac93886284ddb57ea86289d7
Andras <palfi.andras89@gmail.com>
2018-05-23 13:53:04 +0200

    feat(DiceSet): Dices class

---
+++ week-03/day-03/DiceSet/DiceSet/DiceSet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace DiceSet
+{
+    public class Dice
+    {
+        // You have a `DiceSet` class which has a list for 6 dices
+        // You can roll all of them with roll()
+        // Check the current rolled numbers with getCurrent()
+        // You can reroll with reroll()
+        // Your task is to roll the dices until all of the dices are 6
+        static Random RandomValue = new Random();
+
+        static int[] Dices = new int[6];
+
+        public int[] Roll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = RandomValue.Next(1, 7);
+            }
+            return Dices;
+        }
+
+        public int[] GetCurrent()
+        {
+            return Dices;
+        }
+
+        public int GetCurrent(int i)
+        {
+            return Dices[i];
+        }
+
+        public void Reroll()
+        {
+            for (int i = 0; i < Dices.Length; i++)
+            {
+                Dices[i] = RandomValue.Next(1, 7);
+            }
+        }
+
+        public void Reroll(int k)
+        {
+            Dices[k] = new Random().Next(1, 7);
+        }
+    }
+}
--- week-03/day-03/DiceSet/DiceSet/Program.cs
+++ week-03/day-03/DiceSet/DiceSet/Program.cs
@@ -10,6 +10,42 @@ namespace DiceSet
     {
         static void Main(string[] args)
         {
+            /*
+            Random RandomValue = new Random();
+            Dice myDice = new Dice();
+            myDice.GetCurrent();
+            myDice.Roll();
+            myDice.GetCurrent();
+            myDice.GetCurrent(5);
+            myDice.Reroll();
+            myDice.GetCurrent();
+            myDice.Reroll(4);
+            myDice.GetCurrent();*/
+
+            Dice handOfDice = new Dice();
+            
+            var dices = handOfDice.Roll();
+
+            int sum;
+
+            do
+            {
+                sum = 0;
+
+                foreach (int item in dices)
+                {
+                    Console.Write($"[{item}] ");
+                    sum += item;
+                }
+
+                Console.WriteLine();
+
+                
+                dices = handOfDice.Roll();
+
+            } while (sum != dices.Length * 6);
+
+            Console.ReadLine();
         }
     }
 }
