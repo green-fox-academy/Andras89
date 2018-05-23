0d08d4788014771d8fabe99bb8eedc3c7cac2181
Andras <palfi.andras89@gmail.com>
2018-05-23 11:24:59 +0200

    feat(Domino): Domino class

---
+++ week-03/day-03/Domino/Domino/Domino.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domino
+{
+    public class Domino
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+    }
+}
