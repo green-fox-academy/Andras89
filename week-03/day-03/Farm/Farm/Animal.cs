6351b3706ad62f2df05eebcc2937d61eeb5df718
Andras <palfi.andras89@gmail.com>
2018-05-23 20:07:57 +0200

    feat(Farm):Breed(), Slaugther()

--- week-03/day-03/Farm/Farm/Animal.cs
+++ week-03/day-03/Farm/Farm/Animal.cs
@@ -8,5 +8,38 @@ namespace Farm
 {
     class Animal
     {
+        private int Hunger;
+        private int Thirst;
+
+        public Animal()
+        {
+            Hunger = 50;
+            Thirst = 50;
+        }
+
+        public void Eat()
+        {
+            Hunger--;
+        }
+
+        public void Drink()
+        {
+            Thirst--;
+        }
+
+        public void Play()
+        {
+            Hunger++;
+            Thirst++;
+        }
+
+        public int[] AnimalData()
+        {
+            int[] animalData = new int[2];
+            animalData[0] = Hunger;
+            animalData[1] = Thirst;
+
+            return animalData;
+        }
     }
 }
--- week-03/day-03/Farm/Farm/Farm.cs
+++ week-03/day-03/Farm/Farm/Farm.cs
@@ -8,5 +8,38 @@ namespace Farm
 {
     class Farm
     {
+        List<Animal> FarmAnimals = new List<Animal>();
+        private int Slots;
+
+        public Farm()
+        {
+            Slots = 10;
+        }
+
+        public void Slaugther()
+        {
+            int minIndex = 0;
+            int lowestHunger = FarmAnimals[0].AnimalData()[0];
+
+            for (int i = 0; i < FarmAnimals.Count; i++)
+            {
+                if(FarmAnimals[i].AnimalData()[0] < lowestHunger)
+                {
+                    minIndex = i;
+                    lowestHunger = FarmAnimals[i].AnimalData()[0];
+                }
+            }
+
+            FarmAnimals.RemoveAt(minIndex);
+        }
+
+        public void Breed()
+        {
+            if(Slots > 0)
+            {
+                FarmAnimals.Add(new Animal());
+                Slots--;
+            }
+        }
     }
 }
