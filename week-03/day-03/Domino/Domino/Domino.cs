1f02db2683065449d7e770dbac17323ef4429710
Andras <palfi.andras89@gmail.com>
2018-05-23 11:55:38 +0200

    feat(Domino): Domino class

---
+++ week-03/day-03/Domino/Domino/Domino.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Domino
+{
+    public class Domino
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+    }
+}
--- week-03/day-03/Domino/Domino/Program.cs
+++ week-03/day-03/Domino/Domino/Program.cs
@@ -8,8 +8,21 @@ namespace Domino
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var dominoes = InitializeDominoes();
+        }
+
+        public static List<Domino> InitializeDominoes()
+        {
+            var dominoes = new List<Domino>();
+            dominoes.Add(new Domino(5, 2));
+            dominoes.Add(new Domino(4, 6));
+            dominoes.Add(new Domino(1, 5));
+            dominoes.Add(new Domino(6, 7));
+            dominoes.Add(new Domino(2, 4));
+            dominoes.Add(new Domino(7, 1));
+            return dominoes;
         }
     }
 }
