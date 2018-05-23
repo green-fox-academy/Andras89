a4e08898f1e6ec86a1b183cbb782408a4eaeb476
Andras <palfi.andras89@gmail.com>
2018-05-23 15:19:44 +0200

    refactor(Animal): added tests

--- week-03/day-03/Animal/Animal/Animal.cs
+++ week-03/day-03/Animal/Animal/Animal.cs
@@ -8,5 +8,46 @@ namespace Animal
 {
     class Animal
     {
+        /*
+         Every animal has a hunger value, which is a whole number
+        Every animal has a thirst value, which is a whole number
+        when creating a new animal object these values are created with the default 50 value
+        Every animal can eat() which decreases their hunger by one
+        Every animal can drink() which decreases their thirst by one
+        Every animal can play() which increases both by one*/
+
+        private int Hunger;
+        private int Thirst;
+
+        public Animal()
+        {
+            Hunger = 50;
+            Thirst = 50;
+        }
+
+        public void Eat()
+        {
+            Hunger--;
+        }
+
+        public void Drink()
+        {
+            Thirst--;
+        }
+
+        public void Play()
+        {
+            Hunger++;
+            Thirst++;
+        }
+
+        public int[] AnimalData()
+        {
+            int[] animalData = new int[2];
+            animalData[0] = Hunger;
+            animalData[1] = Thirst;
+
+            return animalData;
+        }
     }
 }
--- week-03/day-03/Animal/Animal/Program.cs
+++ week-03/day-03/Animal/Animal/Program.cs
@@ -10,6 +10,15 @@ namespace Animal
     {
         static void Main(string[] args)
         {
+            Animal elephant = new Animal();
+
+            for (int i = 0; i < 30; i++)
+            {
+                elephant.Eat();
+            }
+
+            Console.WriteLine(elephant.AnimalData()[0]);
+            Console.ReadLine();
         }
     }
 }
