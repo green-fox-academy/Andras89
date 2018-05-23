2ad79124cab07701d48365bcf80805746d35c4dc
Andras <palfi.andras89@gmail.com>
2018-05-23 14:01:47 +0200

    chore(FleetOfThings)

---
+++ week-03/day-03/FleetOFThings/FleetOFThings/Fleet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FleetOfThings
+{
+    public class Fleet
+    {
+        private List<Thing> Things;
+
+        public Fleet()
+        {
+            Things = new List<Thing>();
+        }
+
+        public void Add(Thing thing)
+        {
+            Things.Add(thing);
+        }
+    }
+}
