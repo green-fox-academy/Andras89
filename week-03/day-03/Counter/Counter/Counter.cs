a0de2e41726db4179bfe3001fa83b94e5a0afa48
Andras <palfi.andras89@gmail.com>
2018-05-23 15:36:18 +0200

    feat(Counter): Counter class

--- week-03/day-03/Counter/Counter/Counter.cs
+++ week-03/day-03/Counter/Counter/Counter.cs
@@ -8,6 +8,37 @@ namespace Counter
 {
     class Counter
     {
+        private int Sum;
 
+        public Counter()
+        {
+            Sum = 0;
+        }
+
+        public Counter(int Sum)
+        {
+            this.Sum = Sum;
+        }
+
+        public void Add(int number)
+        {
+            Sum += number;
+        }
+
+        public void Add()
+        {
+            Sum++;
+        }
+
+        public string Get()
+        {
+            string result = Sum.ToString();
+            return result;
+        }
+
+        public void Reset()
+        {
+            Sum = 0;
+        }
     }
 }
