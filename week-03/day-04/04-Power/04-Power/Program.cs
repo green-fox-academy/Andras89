30d59b927d86186e342fa5e25d0eb3bdef684e15
Andras <palfi.andras89@gmail.com>
2018-05-24 10:39:14 +0200

    chore(Power)

---
+++ week-03/day-04/04-Power/04-Power/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given base and n that are both 1 or more, compute recursively (no loops)
+            // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+        }
+    }
+}
