ee093ec0a29336147b7b629d37407c2d0811de19
Andras <palfi.andras89@gmail.com>
2018-05-24 11:17:18 +0200

    chore(BunniesAgain)

---
+++ week-03/day-04/06-BunniesAgain/06-BunniesAgain/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_BunniesAgain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+            // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+            // have 3 ears, because they each have a raised foot. Recursively return the
+            // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+        }
+    }
+}
