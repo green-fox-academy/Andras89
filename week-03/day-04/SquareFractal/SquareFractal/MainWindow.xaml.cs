f577e3e14768e9b1dfae197196eef78dadf12c23
Andras <palfi.andras89@gmail.com>
2018-05-24 17:35:20 +0200

    Chore(SquareFractal)

---
+++ week-03/day-04/SquareFractal/SquareFractal/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using GreenFox;
+
+namespace Drawing
+{
+    public partial class MainWindow : Window
+    {
+        FoxDraw foxDraw;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            foxDraw = new FoxDraw(canvas);
+
+            Point upperCoord = new Point(0,0);
+            Point lowerCoord = new Point(canvas.Width, canvas.Height);
+
+            Fractal(upperCoord, lowerCoord);
+        }
+
+        public void Fractal(Point upperCoord, Point lowerCoord)
+        {
+            foxDraw = new FoxDraw(canvas);
+
+
+        }
+    }
+}
