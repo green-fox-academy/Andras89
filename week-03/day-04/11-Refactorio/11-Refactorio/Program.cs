b0b8570d8abdf1ddf7e4793d7465172ff8de37c1
Andras <palfi.andras89@gmail.com>
2018-05-24 13:01:45 +0200

    Chore(Refactorio)

---
+++ week-03/day-04/11-Refactorio/11-Refactorio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Refactorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a recursive function called `refactorio`
+            // that returns it's input's factorial
+
+            int number = 3;
+
+            Console.WriteLine(Refactor(number));
+            Console.ReadLine();
+        }
+
+        public static int Refactor(int number)
+        {
+            if(number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Refactor(number - 1);
+            }
+        }
+    }
+}
