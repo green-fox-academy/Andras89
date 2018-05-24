73062efdc4155fd338f4b7aa976141eab4989901
Andras <palfi.andras89@gmail.com>
2018-05-24 10:38:12 +0200

    feat(SumDigit): SumDigit()

--- week-03/day-04/03-SumDigit/03-SumDigit/Program.cs
+++ week-03/day-04/03-SumDigit/03-SumDigit/Program.cs
@@ -10,7 +10,30 @@ namespace _03_SumDigit
     {
         static void Main(string[] args)
         {
+            // Given a non-negative int n, return the sum of its digits recursively (no loops). 
+            // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+            // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+            int number = int.Parse(Console.ReadLine());
 
+            Console.WriteLine(SumDigit(number));
+
+            Console.ReadLine();
+        }
+
+        public static int SumDigit(int number)
+        {
+            int sum = 0;
+
+            if (number / 10 == 0)
+            {
+                return (number % 10);
+            }
+            else
+            {
+                sum = (number % 10);
+
+                return (sum + SumDigit(number / 10));
+            }
         }
     }
 }
