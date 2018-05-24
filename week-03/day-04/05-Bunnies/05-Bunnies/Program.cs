79acd88f5241eca2f9ec8057d5aa11ec82488ec3
Andras <palfi.andras89@gmail.com>
2018-05-24 11:06:58 +0200

    chore(Bunnies)

---
+++ week-03/day-04/05-Bunnies/05-Bunnies/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies
+            //recursively (without loops or multiplication).
+
+        }
+    }
+}
