9c1b6f9362ee2cc05ebda79fac0d3d74b5ed5e6b
Andras <palfi.andras89@gmail.com>
2018-05-24 11:05:44 +0200

    feat(Power): Power()

--- week-03/day-04/04-Power/04-Power/Program.cs
+++ week-03/day-04/04-Power/04-Power/Program.cs
@@ -12,6 +12,25 @@ namespace _04_Power
         {
             // Given base and n that are both 1 or more, compute recursively (no loops)
             // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+            int baseNum = 3;
+            int expNum = 3;
+
+            Console.WriteLine(Power(baseNum, expNum));
+
+            Console.ReadLine();
+        }
+
+        public static int Power(int baseNum, int expNum)
+        {
+            if (expNum == 1)
+            {
+                return baseNum;
+            }
+            else
+            {
+                return baseNum * Power(baseNum, expNum - 1);
+            }
         }
     }
 }
