0aadf6361df63e2a58bb8b8f44f20a0d52faf890
Andras <palfi.andras89@gmail.com>
2018-05-24 11:16:15 +0200

    feat(Bunnies): BunnyEarCounter()

--- week-03/day-04/05-Bunnies/05-Bunnies/Program.cs
+++ week-03/day-04/05-Bunnies/05-Bunnies/Program.cs
@@ -14,6 +14,25 @@ namespace _05_Bunnies
             // We want to compute the total number of ears across all the bunnies
             //recursively (without loops or multiplication).
 
+            int numberOfBunnies = 8;
+
+            Console.WriteLine(BunnyEarCounter(numberOfBunnies));
+
+            Console.ReadLine();
+        }
+
+        public static int BunnyEarCounter(int numberOfBunnies)
+        {
+            int sum = 0;
+            if (numberOfBunnies == 1)
+            {
+                return 2;
+            }
+            else
+            {
+                sum += 2;
+                return sum + BunnyEarCounter(numberOfBunnies - 1);
+            }
         }
     }
 }
