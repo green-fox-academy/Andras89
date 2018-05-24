89e6df266a6c8bd4e9ff745a706a7fd465e542d9
Andras <palfi.andras89@gmail.com>
2018-05-24 11:28:36 +0200

    feat(BunniesAgain): BunnieEarCounter()

--- week-03/day-04/06-BunniesAgain/06-BunniesAgain/Program.cs
+++ week-03/day-04/06-BunniesAgain/06-BunniesAgain/Program.cs
@@ -14,6 +14,43 @@ namespace _06_BunniesAgain
             // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
             // have 3 ears, because they each have a raised foot. Recursively return the
             // number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+            int numberOfRodents = 3;
+
+            Console.WriteLine(BunnieEarCounter(numberOfRodents));
+
+            Console.ReadLine();
+        }
+
+        public static int BunnieEarCounter(int numberOfRodents)
+        {
+            int sum = 0;
+
+            if (numberOfRodents == 2)
+            {
+                sum += 3;
+
+                return sum + BunnieEarCounter(numberOfRodents - 1);
+            }
+            else if (numberOfRodents == 1)
+            {
+                return 2;
+            }
+            else
+            {
+                if(numberOfRodents % 2 == 1)
+                {
+                    sum += 2;
+
+                    return sum + BunnieEarCounter(numberOfRodents - 1);
+                }
+                else
+                {
+                    sum += 3;
+
+                    return sum + BunnieEarCounter(numberOfRodents - 1);
+                }
+            }
         }
     }
 }
