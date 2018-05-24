12206c5f691e4d358964d2ae37c94705ec3d1737
Andras <palfi.andras89@gmail.com>
2018-05-24 11:41:09 +0200

    chore(Strings)

---
+++ week-03/day-04/07-Strings/07-Strings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+
+            string input = "xxxxxx";
+
+            Console.WriteLine(ConvertxToy());
+
+            Console.ReadLine();
+        }
+
+        public static int ConvertxToy()
+        {
+            
+        }
+    }
+}
