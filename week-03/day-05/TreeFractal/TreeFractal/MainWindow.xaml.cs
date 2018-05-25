03d7a504ed833ebaabdbd988a190167007e84ac3
Andras <palfi.andras89@gmail.com>
2018-05-25 10:45:26 +0200

    chore(TreeFractal)

---
+++ week-03/day-05/TreeFractal/TreeFractal/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using GreenFox;
+
+namespace Drawing
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var foxDraw = new FoxDraw(canvas);
+
+        }
+    }
+}
