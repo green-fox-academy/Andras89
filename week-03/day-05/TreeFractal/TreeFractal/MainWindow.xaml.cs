d98813fb3702676cf85dff99980c8287a423e87d
Andras <palfi.andras89@gmail.com>
2018-05-25 11:57:50 +0200

    feat(TreeFractal): DrawTree()

--- week-03/day-05/TreeFractal/TreeFractal/MainWindow.xaml.cs
+++ week-03/day-05/TreeFractal/TreeFractal/MainWindow.xaml.cs
@@ -9,10 +9,39 @@ namespace Drawing
 {
     public partial class MainWindow : Window
     {
+        FoxDraw foxDraw;
+
+
         public MainWindow()
         {
             InitializeComponent();
-            var foxDraw = new FoxDraw(canvas);
+            double pi = Math.PI;
+            DrawTree(new Point(canvas.Width / 2, canvas.Height / 3 * 2), 6, 80, pi/2, pi/3, 0.8);
+        }
+
+        public void DrawTree(Point startPoint, int depth, double length, double theta,
+                             double deltaTheta, double lengthScale)
+        {
+            foxDraw = new FoxDraw(canvas);
+
+            foxDraw.BackgroundColor(Colors.DarkBlue);
+
+            Point newPoint = new Point();
+            newPoint.X = startPoint.X + length * Math.Cos(theta);
+            newPoint.Y = startPoint.Y - length * Math.Sin(theta);
+
+            foxDraw.DrawLine(startPoint, newPoint);
+
+            if (depth > 1)
+            {
+                DrawTree(newPoint, depth - 1, length * lengthScale, theta + deltaTheta, deltaTheta, lengthScale);
+
+                DrawTree(newPoint, depth - 1, length * lengthScale, theta - deltaTheta, deltaTheta, lengthScale);
+
+                DrawTree(newPoint, depth - 1, length * lengthScale, theta, deltaTheta, lengthScale);
+
+            }
+
 
         }
     }
