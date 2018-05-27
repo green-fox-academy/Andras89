898dff8195a056c7fc97ac5fc349a32e00ecc796
Andras <palfi.andras89@gmail.com>
2018-05-27 21:14:24 +0200

    feat(Hexagon): DrawHexagon()

--- week-03/day-05/Hexagon/Hexagon/MainWindow.xaml.cs
+++ week-03/day-05/Hexagon/Hexagon/MainWindow.xaml.cs
@@ -12,8 +12,35 @@ namespace Drawing
         public MainWindow()
         {
             InitializeComponent();
-            var foxDraw = new FoxDraw(canvas);
 
+            Point centerCoords = new Point(canvas.Width / 2, canvas.Height / 2);
+            double hexaHeight = 600;
+
+            DrawHexagon(centerCoords, hexaHeight);
+
+        }
+
+        public void DrawHexagon(Point centerCoords, double hexaWholeHeight)
+        {
+            FoxDraw foxDraw = new FoxDraw(canvas);
+
+            double hexaHeight = hexaWholeHeight / 2;
+            double aSide = Math.Sqrt(4 / 3 * hexaHeight * hexaHeight);
+            double radius = 2 * aSide / Math.Sqrt(3);
+
+            List<Point> polygonPoints = new List<Point>();
+
+            foxDraw.StrokeColor(Colors.Purple);
+            foxDraw.FillColor(Colors.Transparent);
+
+            polygonPoints.Add(new Point(centerCoords.X - radius, centerCoords.Y));
+            polygonPoints.Add(new Point(centerCoords.X - aSide / 2, centerCoords.Y - hexaHeight));
+            polygonPoints.Add(new Point(centerCoords.X + aSide / 2, centerCoords.Y - hexaHeight));
+            polygonPoints.Add(new Point(centerCoords.X + radius, centerCoords.Y));
+            polygonPoints.Add(new Point(centerCoords.X + aSide / 2, centerCoords.Y + hexaHeight));
+            polygonPoints.Add(new Point(centerCoords.X - aSide / 2, centerCoords.Y + hexaHeight));
+
+            foxDraw.DrawPolygon(polygonPoints);
         }
     }
 }
