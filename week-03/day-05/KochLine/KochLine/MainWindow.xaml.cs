2ceaf860f4dd760854e393c7fbaa00e257947ba7
Andras <palfi.andras89@gmail.com>
2018-05-27 14:53:38 +0200

    chore(KochLine)

---
+++ week-03/day-05/KochLine/KochLine/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using GreenFox;
+
+namespace Drawing
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Point start = new Point(0, canvas.Height / 2);
+            Point end = new Point(canvas.Width, canvas.Height / 2);
+
+            DrawKochLine(start, end);
+        }
+
+        public void DrawKochLine(Point start, Point end)
+        {
+            
+        }
+    }
+}
