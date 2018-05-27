2b5a577ee652ef61d744d7294e6e830cab17311c
Andras <palfi.andras89@gmail.com>
2018-05-27 19:39:28 +0200

    chore(Circles)

---
+++ week-03/day-05/Circles/Circles/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using GreenFox;
+
+namespace Drawing
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            double startX = canvas.Width / 2;
+            double startY = canvas.Height / 2;
+
+            double radius = 300;
+            int depth = 1;
+
+            DrawCircleRecurse(startX, startY, radius, radius, depth);
+        }
+
+        private void DrawCircleRecurse(double startX, double startY, double radius1, double radius2, int depth)
+        {
+        }
+    }
+}
