3282e3d1f8c307b6ce87d0abea991fde921f779f
Andras <palfi.andras89@gmail.com>
2018-05-27 14:08:20 +0200

    feat(EIBWP): WarApp.UltimatePirateShipBattle()

--- week-03/day-03/EverythingIsBetterWithPirates/EverythingIsBetterWithPirates/WarApp.cs
+++ week-03/day-03/EverythingIsBetterWithPirates/EverythingIsBetterWithPirates/WarApp.cs
@@ -8,6 +8,36 @@ namespace EverythingIsBetterWithPirates
 {
     public class WarApp
     {
+        private List<Armada> War = new List<Armada>();
 
+        public WarApp()
+        {
+            War.Add(new Armada());
+            War.Add(new Armada());
+        }
+
+        public string FightUltimatePirateFleetBattle()
+        {
+            string output = String.Empty;
+
+            bool outcome = War[0].Battle(War[1]);
+
+            if (outcome)
+            {
+                output += $"The winner is the First Fleet. \n" +
+                          $"Ship statistics: \n\n" +
+                          $"{War[0].ArmadaStatus()}";
+
+                return output;
+            }
+            else
+            {
+                output += $"The winner is the Second Fleet. \n" +
+                          $"Ship statistics: \n\n" +
+                          $"{War[1].ArmadaStatus()}";
+
+                return output;
+            }
+        }
     }
 }
