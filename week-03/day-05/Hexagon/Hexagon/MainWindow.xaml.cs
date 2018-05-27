18064a60e7b8f351ff90d1d76b5e76376ff3f39e
Andras <palfi.andras89@gmail.com>
2018-05-27 22:01:36 +0200

    feat(Hexagon): HexaRecursion()

--- week-03/day-05/Hexagon/Hexagon/MainWindow.xaml.cs
+++ week-03/day-05/Hexagon/Hexagon/MainWindow.xaml.cs
@@ -14,9 +14,40 @@ namespace Drawing
             InitializeComponent();
 
             Point centerCoords = new Point(canvas.Width / 2, canvas.Height / 2);
-            double hexaHeight = 600;
+            double hexaWholeHeight = 600;
 
-            DrawHexagon(centerCoords, hexaHeight);
+            int depth = 6;
+
+            HexaRecursion(centerCoords, hexaWholeHeight, depth);
+        }
+
+        public void HexaRecursion(Point centerCoords, double hexaWholeHeight, int depth)
+        {
+            DrawHexagon(centerCoords, hexaWholeHeight);
+
+            double hexaHeight = hexaWholeHeight / 2;
+            double aSide = Math.Sqrt(4 / 3 * hexaHeight * hexaHeight);
+
+            if (depth > 1)
+            {
+                Point first = new Point(centerCoords.X - aSide / 3 * 2, centerCoords.Y);
+                HexaRecursion(first, hexaWholeHeight / 3, depth - 1);
+
+                Point second = new Point(centerCoords.X - aSide / 3, centerCoords.Y - hexaWholeHeight / 3);
+                HexaRecursion(second, hexaWholeHeight / 3, depth - 1);
+
+                Point third = new Point(centerCoords.X + aSide / 3, centerCoords.Y - hexaWholeHeight / 3);
+                HexaRecursion(third, hexaWholeHeight / 3, depth - 1);
+
+                Point fourth = new Point(centerCoords.X + aSide / 3 * 2, centerCoords.Y);
+                HexaRecursion(fourth, hexaWholeHeight / 3, depth - 1);
+
+                Point fifth = new Point(centerCoords.X + aSide / 3, centerCoords.Y + hexaWholeHeight / 3);
+                HexaRecursion(fifth, hexaWholeHeight / 3, depth - 1);
+
+                Point sixth = new Point(centerCoords.X - aSide / 3, centerCoords.Y + hexaWholeHeight / 3);
+                HexaRecursion(sixth, hexaWholeHeight / 3, depth - 1); 
+            }
 
         }
 
@@ -26,17 +57,16 @@ namespace Drawing
 
             double hexaHeight = hexaWholeHeight / 2;
             double aSide = Math.Sqrt(4 / 3 * hexaHeight * hexaHeight);
-            double radius = 2 * aSide / Math.Sqrt(3);
 
             List<Point> polygonPoints = new List<Point>();
 
             foxDraw.StrokeColor(Colors.Purple);
             foxDraw.FillColor(Colors.Transparent);
 
-            polygonPoints.Add(new Point(centerCoords.X - radius, centerCoords.Y));
+            polygonPoints.Add(new Point(centerCoords.X - aSide, centerCoords.Y));
             polygonPoints.Add(new Point(centerCoords.X - aSide / 2, centerCoords.Y - hexaHeight));
             polygonPoints.Add(new Point(centerCoords.X + aSide / 2, centerCoords.Y - hexaHeight));
-            polygonPoints.Add(new Point(centerCoords.X + radius, centerCoords.Y));
+            polygonPoints.Add(new Point(centerCoords.X + aSide, centerCoords.Y));
             polygonPoints.Add(new Point(centerCoords.X + aSide / 2, centerCoords.Y + hexaHeight));
             polygonPoints.Add(new Point(centerCoords.X - aSide / 2, centerCoords.Y + hexaHeight));
 
