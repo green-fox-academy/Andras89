669ce9cbf8219c726b8bb6ba750fac59fbcfb55b
Andras <palfi.andras89@gmail.com>
2018-05-27 20:57:51 +0200

    feat(Circles): DrawCirclesRecurse()

--- week-03/day-05/Circles/Circles/MainWindow.xaml.cs
+++ week-03/day-05/Circles/Circles/MainWindow.xaml.cs
@@ -13,17 +13,42 @@ namespace Drawing
         {
             InitializeComponent();
 
-            double startX = canvas.Width / 2;
-            double startY = canvas.Height / 2;
+            double startX = 0;
+            double startY = 0;
 
-            double radius = 300;
-            int depth = 1;
+            double radius = 600;
+            int depth = 2;
 
             DrawCircleRecurse(startX, startY, radius, radius, depth);
         }
 
         private void DrawCircleRecurse(double startX, double startY, double radius1, double radius2, int depth)
         {
+            DrawCircle(startX, startY, radius1, radius1);
+
+            double middleUpX = radius1 / 4;
+            double middleUpY = startY;
+
+            double leftRightDownY = radius1 / 2 - Math.Sin(Math.PI / 6) * radius1 / 4;
+            double leftDownX =radius1 / 4 - Math.Cos(Math.PI / 6) * (radius1 / 4);
+            double rightDownX = radius1 / 2 - radius1 / 4 + Math.Cos(Math.PI / 6) * (radius1 / 4);
+
+            if (depth > 1)
+            {
+                DrawCircleRecurse(middleUpX, middleUpY, radius1 / 2, radius2 / 2, depth - 1);
+                DrawCircleRecurse(leftDownX, leftRightDownY, radius1 / 2, radius2 / 2, depth - 1);
+                DrawCircleRecurse(rightDownX, leftRightDownY, radius1 / 2, radius2 / 2, depth - 1); 
+            }
+        }
+
+        public void DrawCircle(double startX, double startY, double radius1, double radius2)
+        {
+            FoxDraw foxDraw = new FoxDraw(canvas);
+
+            foxDraw.StrokeColor(Colors.Orange);
+            foxDraw.FillColor(Colors.Transparent);
+
+            foxDraw.DrawEllipse(startX, startY, radius1, radius1);
         }
     }
 }
