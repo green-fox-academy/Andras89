9b34160444c21f788d5553fc7ea7f765dc110a7d
Andras <palfi.andras89@gmail.com>
2018-05-27 19:29:40 +0200

    feat(Triangles): DrawTriangleCarpet()

--- week-03/day-05/Triangles/Triangles/MainWindow.xaml.cs
+++ week-03/day-05/Triangles/Triangles/MainWindow.xaml.cs
@@ -12,7 +12,51 @@ namespace Drawing
         public MainWindow()
         {
             InitializeComponent();
-            var foxDraw = new FoxDraw(canvas);
+            double aSide = 500;
+            Point center = new Point(300, 300);
+            int depth = 4;
+
+            DrawTriangleCarpet(aSide, center, depth);
+        }
+
+        private void DrawTriangleCarpet(double aSide, Point center, int depth)
+        {
+            DrawTriangle(aSide, center);
+
+            double height = aSide * Math.Sqrt(3) / 2;
+
+            Point leftUp = new Point(center.X - aSide / 4, center.Y - height / 6);
+            Point rightUp = new Point(center.X + aSide / 4, center.Y - height / 6);
+            Point middleDown = new Point(center.X, center.Y + height / 3);
+
+            if (depth > 1)
+            {
+                DrawTriangleCarpet(aSide / 2, leftUp, depth - 1);
+                DrawTriangleCarpet(aSide / 2, rightUp, depth - 1);
+                DrawTriangleCarpet(aSide / 2, middleDown, depth - 1);
+            }
+        }
+
+        public void DrawTriangle(double aSide, Point center)
+        {
+            FoxDraw foxDraw = new FoxDraw(canvas);
+
+            foxDraw.FillColor(Colors.Transparent);
+            foxDraw.StrokeColor(Colors.Purple);
+
+            double height = aSide * Math.Sqrt(3) / 2;
+
+
+            Point leftPoint = new Point(center.X - aSide / 2, center.Y - height / 3);
+            Point rightPoint = new Point(center.X + aSide / 2, center.Y - height / 3);
+            Point middlePoint = new Point(center.X, center.Y + height * 2 / 3);
+
+            List<Point> poly = new List<Point>();
+            poly.Add(leftPoint);
+            poly.Add(rightPoint);
+            poly.Add(middlePoint);
+
+            foxDraw.DrawPolygon(poly);
 
         }
     }
