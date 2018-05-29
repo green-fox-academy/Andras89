53206000e605920394ece0f8fb6b0e426307c79d
Andras <palfi.andras89@gmail.com>
2018-05-29 15:03:18 +0200

    feat(Carrier): F16 class

---
+++ week-04/day-02/Carrier/Carrier/F16.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carrier
+{
+    public class F16 : Aircraft
+    {
+        public F16()
+        {
+            BaseDamage = 30;
+            MaxAmmo = 8;
+        }
+    }
+}
