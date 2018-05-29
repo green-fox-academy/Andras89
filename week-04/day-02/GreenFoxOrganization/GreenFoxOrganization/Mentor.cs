d3b2c8ff7509889e6632e37cb240e41566def21e
Andras <palfi.andras89@gmail.com>
2018-05-29 12:58:14 +0200

    feat(GreenFoxOrganization): Mentor.GetGoal(), Introduce()

--- week-04/day-02/GreenFoxOrganization/GreenFoxOrganization/Mentor.cs
+++ week-04/day-02/GreenFoxOrganization/GreenFoxOrganization/Mentor.cs
@@ -6,8 +6,28 @@ using System.Threading.Tasks;
 
 namespace GreenFoxOrganization
 {
-    public class Mentor
+    public class Mentor : Person
     {
+        private string level;
 
+        public Mentor(string name, int age, string sex, string level) : base(name, age, sex)
+        {
+            this.level = level;
+        }
+
+        public Mentor()
+        {
+            level = "intermediate";
+        }
+
+        public override string Introduce()
+        {
+            return String.Format("Hi, I'm {0}, a {1} year old {2} {3} mentor.", Name, Age, Sex, level);
+        }
+
+        public override string GetGoal()
+        {
+            return "Educate brilliant junior software developers.";
+        }
     }
 }
