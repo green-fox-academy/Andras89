2f250f6895db49af23454525a9d635e42765e1e4
Andras <palfi.andras89@gmail.com>
2018-05-29 12:19:59 +0200

    feat(GreenFoxOrganization): Person. Introduce(), GetGoal()

--- week-04/day-02/GreenFoxOrganization/GreenFoxOrganization/Person.cs
+++ week-04/day-02/GreenFoxOrganization/GreenFoxOrganization/Person.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace GreenFoxOrganization
 {
-    class Person
+    public class Person
     {
+        private string name;
+        private int age;
+        private string sex;
+
+        public Person(string name, int age, string sex)
+        {
+            this.name = name;
+            this.age = age;
+            this.sex = sex;
+        }
+
+        public Person()
+        {
+            name = "Jane Doe";
+            age = 30;
+            sex = "female";
+        }
+
+        public string Introduce()
+        {
+            return String.Format("Hi, I'm {0}, a {1} year old {2}.", name, age, sex);
+        }
+
+        public virtual GetGoal()
+        {
+            return "My goal is: Live for the moment!";
+        }
     }
 }
