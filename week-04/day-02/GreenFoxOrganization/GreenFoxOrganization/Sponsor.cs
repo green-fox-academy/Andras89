cb953f0bb805f7613089f5124eaec25324ee1054
Andras <palfi.andras89@gmail.com>
2018-05-29 13:06:15 +0200

    feat(GreenFoxOrganization): Sponsor.Getgoal()

--- week-04/day-02/GreenFoxOrganization/GreenFoxOrganization/Sponsor.cs
+++ week-04/day-02/GreenFoxOrganization/GreenFoxOrganization/Sponsor.cs
@@ -6,8 +6,28 @@ using System.Threading.Tasks;
 
 namespace GreenFoxOrganization
 {
-    class Sponsor
+    public class Sponsor : Person
     {
+        private string company;
+        private int studentsHired;
 
+        public Sponsor()
+        {
+            company = "Google";
+            studentsHired = 0;
+        }
+
+        public Sponsor(string name, int age, string sex, string company) : base(name, age, sex)
+        {
+            this.company = company;
+            studentsHired = 0;
+        }
+
+
+
+        public override string GetGoal()
+        {
+            return "Hire brilliant junior software developers.";
+        }
     }
 }
