a0ff1b59a614fbfb4fe446020ec19478c4ebf20e
Andras <palfi.andras89@gmail.com>
2018-05-30 22:17:36 +0200

    feat(TestPractice): try catch test

--- week-04/day-04/PracticeApp/TestApp/TestTextHandler.cs
+++ week-04/day-04/PracticeApp/TestApp/TestTextHandler.cs
@@ -24,5 +24,19 @@ namespace TestApp
         {
             Assert.Catch<NotImplementedException>(() => test.MakeLowerCase("boo"));
         }
+
+        [Test]
+        public void TestMakeLowerCaseTwo()
+        {
+            try
+            {
+                test.MakeLowerCase("boo");
+                Assert.Fail();
+            }
+            catch (NotImplementedException)
+            {
+                Assert.Pass();
+            }
+        }
     }
 }
