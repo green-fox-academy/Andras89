8f00c93c6b0402c41dceee0bef51d2b97d441d63
Andras <palfi.andras89@gmail.com>
2018-05-30 15:36:47 +0200

    feat(Flyable): Helicopter

---
+++ week-04/day-03/Flyable/Flyable/Helicopter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyable
+{
+    class Helicopter : Vehicle, IFlyable
+    {
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Land()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeOff()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
