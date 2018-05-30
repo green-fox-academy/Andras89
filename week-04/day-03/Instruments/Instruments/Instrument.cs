7235e655b58e44a800aaeab8a008b125c9c5a6ce
Andras <palfi.andras89@gmail.com>
2018-05-30 14:07:32 +0200

    feat(Instruments): StringedInstrument class

--- week-04/day-03/Instruments/Instruments/Instrument.cs
+++ week-04/day-03/Instruments/Instruments/Instrument.cs
@@ -8,6 +8,10 @@ namespace Instruments
 {
     public abstract class Instrument
     {
+        private string name;
 
+        protected string Name { get => name; set => name = value; }
+
+        public abstract void Play();
     }
 }
