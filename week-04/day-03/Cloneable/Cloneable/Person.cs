80e6d70236b51f60be8dc70bb0a84916d33c7ab4
Andras <palfi.andras89@gmail.com>
2018-05-30 12:00:06 +0200

    chore(Cloneable)

---
+++ week-04/day-03/Cloneable/Cloneable/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloneable
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        private string sex;
+
+        public Person(string name, int age, string sex)
+        {
+            this.name = name;
+            this.age = age;
+            this.sex = sex;
+        }
+
+        public Person()
+        {
+            name = "Jane Doe";
+            age = 30;
+            sex = "female";
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+        }
+
+        public string Sex
+        {
+            get
+            {
+                return sex;
+            }
+        }
+
+        public virtual string Introduce()
+        {
+            return String.Format("Hi, I'm {0}, a {1} year old {2}.", name, age, sex);
+        }
+
+        public virtual string GetGoal()
+        {
+            return "My goal is: Live for the moment!";
+        }
+    }
+}
---
+++ week-04/day-03/Cloneable/Cloneable/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloneable
+{
+    public class Student : Person
+    {
+        private string previousOrganization;
+        private int skippedDays;
+
+        public Student(string name, int age, string sex, string previousOrganization) : base(name, age, sex)
+        {
+            this.previousOrganization = previousOrganization;
+            skippedDays = 0;
+        }
+
+        public Student()
+        {
+            previousOrganization = "The School of Life";
+            skippedDays = 0;
+        }
+
+        public override string Introduce()
+        {
+            return String.Format("Hi, I'm {0}, a {1} year old {2} from {3}" +
+                                 " who skipped {4} days from the course already."
+                                 , Name, Age, Sex, previousOrganization, skippedDays);
+        }
+
+        public override string GetGoal()
+        {
+            return "Be a junior software developer.";
+        }
+
+        public void SkipDays(int DaysToSkip)
+        {
+            skippedDays += DaysToSkip;
+        }
+    }
+}
