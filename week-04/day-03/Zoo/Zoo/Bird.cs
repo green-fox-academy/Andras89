18da090d86a19d630ed5629a277059a5794aa05b
Andras <palfi.andras89@gmail.com>
2018-05-30 15:40:28 +0200

    feat(Zoo): Bird IFlyable

--- week-04/day-03/Zoo/Zoo/Bird.cs
+++ week-04/day-03/Zoo/Zoo/Bird.cs
@@ -6,16 +6,31 @@ using System.Threading.Tasks;
 
 namespace Zoo
 {
-    public class Bird : Animal
+    public class Bird : Animal, IFlyable
     {
         public Bird(string Name)
         {
             this.Name = Name;
         }
 
+        public void Fly()
+        {
+            throw new NotImplementedException();
+        }
+
         public override void Greet()
         {
             Console.WriteLine("Cheerp!");
+        }
+
+        public void Land()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TakeOff()
+        {
+            throw new NotImplementedException();
         }
 
         public override string WantChild()
