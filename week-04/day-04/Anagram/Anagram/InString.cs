fbab03d6b72b7e1abc548f59d99e9f4ff248d96d
Andras <palfi.andras89@gmail.com>
2018-05-31 14:12:09 +0200

    feat(Anagram): InString.AreAnagram

---
+++ week-04/day-04/Anagram/Anagram/InString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    public class InString
+    {
+        public bool AreAnagrams(string first, string second)
+        {
+            char[] temp = second.ToCharArray();
+            temp.Reverse();
+            second = new string(temp);
+
+            return (first.ToLower() == second.ToLower());
+        }
+    }
+}
