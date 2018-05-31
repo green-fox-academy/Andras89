746b81c1da8e4e74ee49d8941769c9cfceef5942
Andras <palfi.andras89@gmail.com>
2018-05-31 12:25:21 +0200

    chore(Sum)

---
+++ week-04/day-04/Sum/Sum/Summer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum
+{
+    public class Summer
+    {
+        public List<int> NumberList { get; set; }
+
+        public int Sum()
+        {
+            int sum = 0;
+
+            foreach ( int current in NumberList)
+            {
+                sum += current;
+            }
+
+            return sum;
+        }
+    }
+}
