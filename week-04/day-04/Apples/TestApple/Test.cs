6d160f5fecf0d08ae88408c8098100f3f75ce716
Andras <palfi.andras89@gmail.com>
2018-05-31 12:09:54 +0200

    chore(Apples)

---
+++ week-04/day-04/Apples/TestApple/Test.cs
@@ -0,0 +1,34 @@
+﻿using Apples;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test
+    {
+        private Apple shineyRedApple;
+
+        [SetUp]
+        public void SetUp()
+        {
+            shineyRedApple = new Apple();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            shineyRedApple = null;
+        }
+
+        [TestCase("apple")]
+        public void Tester(string apple)
+        {
+            Assert.AreEqual(apple, shineyRedApple.GetApple());
+        }
+    }
+}
