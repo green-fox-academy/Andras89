11112e2dd548aaa565f45a0a569bfd9793f233c9
Andras <palfi.andras89@gmail.com>
2018-06-03 20:45:13 +0200

    feat(Fibonacci): more testcases

--- week-04/day-04/Fibonacci/TestEnvironment/Test.cs
+++ week-04/day-04/Fibonacci/TestEnvironment/Test.cs
@@ -26,36 +26,46 @@ namespace Test
         }
 
         [TestCase("0")]
+        [TestCase(0)]
+        [TestCase('0')]
         public void WhatsIsIndexZero(object index)
         {
             Assert.AreEqual(0, fibo.FibonacciMaker(index));
         }
 
         [TestCase("1")]
+        [TestCase(1)]
+        [TestCase('1')]
         public void WhatsIsIndexOne(object index)
         {
             Assert.AreEqual(1, fibo.FibonacciMaker(index));
         }
 
         [TestCase("2")]
+        [TestCase(2)]
+        [TestCase('2')]
         public void WhatsIsIndexTwo(object index)
         {
             Assert.AreEqual(1, fibo.FibonacciMaker(index));
         }
 
         [TestCase("3")]
+        [TestCase(3)]
+        [TestCase('3')]
         public void WhatsIsIndexThree(object index)
         {
             Assert.AreEqual(2, fibo.FibonacciMaker(index));
         }
 
         [TestCase("-1")]
+        [TestCase(-156565)]
         public void WhatsIsIndexMinusOne(object index)
         {
             Assert.AreEqual(0, fibo.FibonacciMaker(index));
         }
 
         [TestCase('a')]
+        [TestCase("dhghFGHfbgf")]
         public void WhatsIsIndexA(object index)
         {
             Assert.Catch<ArgumentException>(() => fibo.FibonacciMaker(index));
