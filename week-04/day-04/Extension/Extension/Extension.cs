f796b4b0059af5accc0f5bbe57eb844e7cef48d0
Andras <palfi.andras89@gmail.com>
2018-06-03 18:32:12 +0200

    chore(Extensions)

---
+++ week-04/day-04/Extension/Extension/Extension.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public class Extension
+    {
+        public int Add(int a, int b)
+        {
+            return 5;
+        }
+
+        public int MaxOfThree(int a, int b, int c)
+        {
+            if (a > b)
+                return a;
+            else
+                return c;
+        }
+
+        public int Median(List<int> pool)
+        {
+            return pool[(pool.Count - 1) / 2];
+        }
+
+        public bool IsVowel(char c)
+        {
+            return (new List<char>() { 'a', 'u', 'o', 'e', 'i' }).Contains(c);
+        }
+
+        public string Translate(string hungarian)
+        {
+            string teve = hungarian;
+            int length = teve.Length;
+            for (int i = 0; i < length; i++)
+            {
+                char c = teve[i];
+                if (IsVowel(c))
+                {
+                    teve = string.Join(c + "v" + c, teve.Split(c));
+                    i += 2;
+                    length += 2;
+                }
+            }
+
+            return teve;
+        }
+    }
+}
---
+++ week-04/day-04/Extension/TestEnvironment/Test.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Extensions;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test
+    {
+        Extension extension = new Extension();
+
+        [Test]
+        public void TestAdd_2and3is5()
+        {
+            Assert.AreEqual(5, extension.Add(2, 3));
+        }
+
+        [Test]
+        public void TestAdd_1and4is5()
+        {
+            Assert.AreEqual(5, extension.Add(1, 4));
+        }
+
+        [Test]
+        public void TestMaxOfThree_First()
+        {
+            Assert.AreEqual(5, extension.MaxOfThree(5, 4, 3));
+        }
+
+        [Test]
+        public void TestMaxOfThree_Fhird()
+        {
+            Assert.AreEqual(5, extension.MaxOfThree(3, 4, 5));
+        }
+
+        [Test]
+        public void TestMedian_Four()
+        {
+            Assert.AreEqual(5, extension.Median(new List<int>() { 7, 5, 3, 5 }));
+        }
+
+        [Test]
+        public void TestMedian_Five()
+        {
+            Assert.AreEqual(3, extension.Median(new List<int>() { 1, 2, 3, 4, 5 }));
+        }
+
+        [Test]
+        public void TestIsVowel_a()
+        {
+            Assert.True(extension.IsVowel('a'));
+        }
+
+        [Test]
+        public void TestIsVowel_u()
+        {
+            Assert.True(extension.IsVowel('u'));
+        }
+
+        [Test]
+        public void testTranslate_bemutatkozik()
+        {
+            Assert.AreEqual("bevemuvutavatkovozivik", extension.Translate("bemutatkozik"));
+        }
+
+        [Test]
+        public void testTranslate_kolbice()
+        {
+            Assert.AreEqual("lavagovopuvus", extension.Translate("lagopus"));
+        }
+    }
+}
