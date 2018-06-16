43ad3038d6bd00edfe73b24909e111eaa1cd3c07
Andras <palfi.andras89@gmail.com>
2018-06-16 17:03:33 +0200

    feat(Program): NumberChecker()

--- week-06/day-01/LINQPractice/LINQPractice/Program.cs
+++ week-06/day-01/LINQPractice/LINQPractice/Program.cs
@@ -24,7 +24,14 @@ namespace LINQPractice
 
             ListAverage(new List<int> { 1, 3, 5, 1 });
 
+            NumberChecker(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
             Console.ReadLine();
+        }
+
+        private static void NumberChecker(List<int> list)
+        {
+
         }
 
         private static void ListAverage(List<int> list)
