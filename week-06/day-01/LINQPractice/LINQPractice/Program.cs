05dce790628fda835ed01bdafa1a4c0b9f658927
Andras <palfi.andras89@gmail.com>
2018-06-16 19:34:24 +0200

    refactor

--- week-06/day-01/LINQPractice/LINQPractice/Program.cs
+++ week-06/day-01/LINQPractice/LINQPractice/Program.cs
@@ -30,6 +30,8 @@ namespace LINQPractice
 
             NumberExceptAndIntersectList(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, new List<int> { 1, 3, 5, 10 });
 
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
             Console.ReadLine();
         }
 
