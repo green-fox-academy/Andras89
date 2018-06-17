a1a226dddc579b59e94c9c6787fa3e7c1d0b77e6
Andras <palfi.andras89@gmail.com>
2018-06-17 11:34:00 +0200

    feat(Exercise3): positive squared

--- week-06/day-01/Exercise3/Exercise3/Program.cs
+++ week-06/day-01/Exercise3/Exercise3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exercise3
 {
@@ -6,7 +7,19 @@ namespace Exercise3
     {
         static void Main(string[] args)
         {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
+            Console.Write(string.Join(", ", n.Where(x => x > 0).Select(x => Math.Pow(x, 2))));
+
+            Console.WriteLine();
+
+            var result = from num in n
+                         where num > 0
+                         select Math.Pow(num, 2);
+
+            Console.Write(string.Join(", ", result));
+
+            Console.ReadLine();
         }
     }
 }
