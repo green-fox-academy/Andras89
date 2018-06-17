946b26241e553f54ead70ceb3360feba6a29ccb5
Andras <palfi.andras89@gmail.com>
2018-06-17 10:55:51 +0200

    chore(Exercise1)

---
+++ week-06/day-01/Excercise1/Excercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Excercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+
+        }
+    }
+}
