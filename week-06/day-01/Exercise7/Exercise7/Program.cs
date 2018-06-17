e238d29036ed7674651ddccf34f09da3f79585d8
Andras <palfi.andras89@gmail.com>
2018-06-17 12:45:03 +0200

    feat(Exercise7): string start A end I

--- week-06/day-01/Exercise7/Exercise7/Program.cs
+++ week-06/day-01/Exercise7/Exercise7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exercise7
 {
@@ -6,7 +7,17 @@ namespace Exercise7
     {
         static void Main(string[] args)
         {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
 
+            Console.Write(string.Join(", ", cities.Where(x => x.StartsWith('A') && x.EndsWith('I'))) + '\n');
+
+            var queryResult = from city in cities
+                              where city[0] == 'A' && city[city.Length - 1] == 'I'
+                              select city;
+
+            Console.WriteLine(string.Join(", ", queryResult));
+
+            Console.ReadLine();
         }
     }
 }
