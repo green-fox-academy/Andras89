0e1f9e87974690a555724e951e5f45fa7ae35031
Andras <palfi.andras89@gmail.com>
2018-06-17 11:58:18 +0200

    feat(Exercise5): frequency of numbers

--- week-06/day-01/Exercise5/Exercise5/Program.cs
+++ week-06/day-01/Exercise5/Exercise5/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Exercise5
 {
@@ -6,7 +8,34 @@ namespace Exercise5
     {
         static void Main(string[] args)
         {
+            int[] n = new int[] { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
 
+            Dictionary<int, int> lambdaResult = n.OrderBy(x => x)
+                                                 .GroupBy(x => x)
+                                                 .ToDictionary(x => x.Key, x => x.ToList().Count);
+
+            foreach (KeyValuePair<int, int> item in lambdaResult)
+            {
+                Console.Write($"{item.Key}: {item.Value}  ");
+            }
+
+            Console.WriteLine();
+
+            var queryResult = from num in n
+                              orderby num
+                              group num by num into sortedGroup
+                              select new
+                              {
+                                  Number = sortedGroup.Key,
+                                  numCount = sortedGroup.Count()
+                              };
+
+            foreach (var item in queryResult)
+            {
+                Console.Write($"{item.Number}: {item.numCount}  ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
