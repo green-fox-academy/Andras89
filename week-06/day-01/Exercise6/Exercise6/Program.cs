c59c549e699d6f122ce549c5895adb5892ca13e6
Andras <palfi.andras89@gmail.com>
2018-06-17 12:17:20 +0200

    feat(Exercise6): input string char frequency

--- week-06/day-01/Exercise6/Exercise6/Program.cs
+++ week-06/day-01/Exercise6/Exercise6/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Exercise6
 {
@@ -6,7 +8,36 @@ namespace Exercise6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Write random string:");
 
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> lambdaResult = input.Where(x => x != ' ')
+                                                      .GroupBy(x => x)
+                                                      .ToDictionary(x => x.Key, x => x.ToList().Count);
+
+            foreach (KeyValuePair<char, int> item in lambdaResult)
+            {
+                Console.Write($"{item.Key}: {item.Value} , ");
+            }
+
+            Console.WriteLine();
+
+            var queryResult = from character in input
+                              where character != ' '
+                              group character by character into grouping
+                              select new
+                              {
+                                  HeadChar = grouping.Key,
+                                  CharCounter = grouping.Count()
+                              };
+
+            foreach (var item in queryResult)
+            {
+                Console.Write($"{item.HeadChar}: {item.CharCounter} , ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
