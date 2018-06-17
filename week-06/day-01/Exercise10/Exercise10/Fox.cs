4fd78a209c5158819d268c99c646ccce95a28768
Andras <palfi.andras89@gmail.com>
2018-06-17 13:47:19 +0200

    feat(Exercise10): Fox class construct and list fill

--- week-06/day-01/Exercise10/Exercise10/Fox.cs
+++ week-06/day-01/Exercise10/Exercise10/Fox.cs
@@ -6,5 +6,15 @@ namespace Exercise10
 {
     public class Fox
     {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+
+        public Fox(string name, string type, string color)
+        {
+            Name = name;
+            Type = type;
+            Color = color;
+        }
     }
 }
--- week-06/day-01/Exercise10/Exercise10/Program.cs
+++ week-06/day-01/Exercise10/Exercise10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Exercise10
 {
@@ -6,7 +7,14 @@ namespace Exercise10
     {
         static void Main(string[] args)
         {
-
+            List<Fox> listOfFoxes = new List<Fox>
+            {
+                new Fox("Pix", "pallida", "green"),
+                new Fox("Vuk", "vipes", "red"),
+                new Fox("Cheesus", "fulvipes", "ocelot"),
+                new Fox("Heki", "macrotis", "pebble"),
+                new Fox("Vox", "pallida", "green"),
+            };
         }
     }
 }
