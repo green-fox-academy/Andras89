43a3ac3b24121436e8403746c516f58faa2ea4c7
Andras <palfi.andras89@gmail.com>
2018-06-17 13:37:15 +0200

    feat(Exercise9): convert char array to string

--- week-06/day-01/Exercise9/Exercise9/Program.cs
+++ week-06/day-01/Exercise9/Exercise9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exercise9
 {
@@ -6,7 +7,18 @@ namespace Exercise9
     {
         static void Main(string[] args)
         {
+            char[] input = new char[] { 'Y', 'o', 'l', 'o' };
 
+            var lambdaResult = string.Join("", input.Select(x => x.ToString()));
+
+            Console.WriteLine(lambdaResult);
+
+            var queryResult = from character in input
+                              select character.ToString();
+
+            Console.WriteLine(string.Join("", queryResult));
+
+            Console.ReadLine();
         }
     }
 }
