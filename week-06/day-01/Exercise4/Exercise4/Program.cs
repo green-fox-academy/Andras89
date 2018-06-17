dbb9263d382c795de9eaecf08c3256da8979fb2b
Andras <palfi.andras89@gmail.com>
2018-06-17 11:41:14 +0200

    feat(Exercise4): more than 20 squared

--- week-06/day-01/Exercise4/Exercise4/Program.cs
+++ week-06/day-01/Exercise4/Exercise4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Exercise4
 {
@@ -6,7 +8,17 @@ namespace Exercise4
     {
         static void Main(string[] args)
         {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
 
+            Console.Write(string.Join(", ", n.Where(x => Math.Pow(x, 2) > 20)) + '\n');
+
+            IEnumerable<int> result = from num in n
+                                      where Math.Pow(num, 2) > 20
+                                      select num;
+
+            Console.Write(string.Join(", ", result));
+
+            Console.ReadLine();
         }
     }
 }
