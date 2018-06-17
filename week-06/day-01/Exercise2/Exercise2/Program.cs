29f10c88df2df51cc0072c20ff6b5cdeeb94b637
Andras <palfi.andras89@gmail.com>
2018-06-17 11:21:59 +0200

    feat(Exercise2): odd average

--- week-06/day-01/Exercise2/Exercise2/Program.cs
+++ week-06/day-01/Exercise2/Exercise2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exercise2
 {
@@ -6,7 +7,17 @@ namespace Exercise2
     {
         static void Main(string[] args)
         {
-            
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            Console.WriteLine(n.Where(x => x % 2 == 1).Average());
+
+            var result = (from num in n
+                          where num % 2 == 1
+                          select num).Average();
+
+            Console.WriteLine(result);
+
+            Console.ReadLine();
         }
     }
 }
