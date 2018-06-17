9b475770db42398080837ea5a0d44927a9cf1506
Andras <palfi.andras89@gmail.com>
2018-06-17 13:19:13 +0200

    feat(Exercise8): input string find UpperCase

--- week-06/day-01/Exercise8/Exercise8/Program.cs
+++ week-06/day-01/Exercise8/Exercise8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Exercise8
 {
@@ -6,7 +7,19 @@ namespace Exercise8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Give a string input:");
 
+            string input = Console.ReadLine();
+
+            Console.Write(string.Join(", ", input.Where(x => x.ToString().ToUpper() == x.ToString()).Except(new char[] { ' ', '!', '%' })) + '\n');
+
+            var queryResult = (from character in input
+                              where character.ToString().Equals(character.ToString().ToUpper())
+                              select character).Except(new char[] { ' ', '!', '%' });
+
+            Console.Write(string.Join(", ", queryResult));
+
+            Console.ReadLine();
         }
     }
 }
