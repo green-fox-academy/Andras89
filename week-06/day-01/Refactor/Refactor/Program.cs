d7050c1a9365047d2ec50b49cecba1bba2faa939
Andras <palfi.andras89@gmail.com>
2018-06-18 10:54:02 +0200

    feat(Refactor): Even numbers

--- week-06/day-01/Refactor/Refactor/Program.cs
+++ week-06/day-01/Refactor/Refactor/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Refactor
 {
@@ -6,7 +9,20 @@ namespace Refactor
     {
         static void Main(string[] args)
         {
+            int[] array = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
 
+            IEnumerable<int> lambdaResult = array.Where(x => x % 2 == 0);
+
+
+            IEnumerable<int> queryResult = from number in array
+                                           where number % 2 == 0
+                                           select number;
+
+            lambdaResult.ToList().ForEach(xout => Console.Write($"{xout}, "));
+            Console.WriteLine();
+            queryResult.ToList().ForEach(xout => Console.Write($"{xout}, "));
+
+            Console.ReadLine();
         }
     }
 }
