c315c730d12cba524e97b7bed0b03432223da26f
Andras <palfi.andras89@gmail.com>
2018-06-24 18:26:51 +0200

    chore(MVCTutorial)

--- week-07/day-01/MVCTutorial/MVCTutorial/Startup.cs
+++ week-07/day-01/MVCTutorial/MVCTutorial/Startup.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Tutorial
+namespace MVCTutorial
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,15 +29,21 @@ namespace Tutorial
         {
             if (env.IsDevelopment())
             {
+                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
-            app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            app.Run((context) =>
+            app.UseMvc(routes =>
             {
-                throw new Exception();
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
