83f6b8030e52afd78ca1ac01f8fda5b30bc1eb4c
Andras <palfi.andras89@gmail.com>
2018-06-24 19:42:24 +0200

    chore(MvcTutorialaApp)

---
+++ week-07/day-01/MvcTutorialApp/MvcTutorialApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcTutorialApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcTutorialApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            Contact contact = new Contact()
+            {
+                firstName = "Andrew",
+                lastName = "Paulson"
+            };
+
+            return View(contact);
+        }
+    }
+}
---
+++ week-07/day-01/MvcTutorialApp/MvcTutorialApp/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcTutorialApp.Models
+{
+    public class Contact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
