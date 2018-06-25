90af1ca87869bb68cd81b8bd37ba0c6a5b85ddbe
Andras <palfi.andras89@gmail.com>
2018-06-25 12:16:44 +0200

    feat(HomeController): hello() + routes

---
+++ week-07/day-01/DailyProject/DailyProject/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyProject.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Hello")]
+        public IActionResult Hello()
+        {
+            return Content("Ey this is a hello method!");
+        }
+    }
+}
--- week-07/day-01/DailyProject/DailyProject/Startup.cs
+++ week-07/day-01/DailyProject/DailyProject/Startup.cs
@@ -15,11 +15,14 @@ namespace DailyProject
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseMvcWithDefaultRoute();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
