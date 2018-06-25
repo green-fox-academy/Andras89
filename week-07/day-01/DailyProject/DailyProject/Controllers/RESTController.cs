6a4e33cc1080dad1d4c093156337671727eb9be7
Andras <palfi.andras89@gmail.com>
2018-06-25 12:29:24 +0200

    feat(RESTController): greet yourself

---
+++ week-07/day-01/DailyProject/DailyProject/Controllers/RESTController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DailyProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyProject.Controllers
+{
+    [Route("Api")]
+    public class RESTController : Controller
+    {
+
+        [Route("Greeting/{name}")]
+        public IActionResult Greeting([FromRoute] string name)
+        {
+            Greeting greet = new Greeting()
+            {
+                Id = 0,
+                Content = "Hello" + name
+            };
+
+            return new JsonResult(greet);
+        }
+    }
+}
--- week-07/day-01/DailyProject/DailyProject/Models/Greeting.cs
+++ week-07/day-01/DailyProject/DailyProject/Models/Greeting.cs
@@ -7,5 +7,7 @@ namespace DailyProject.Models
 {
     public class Greeting
     {
+        public long Id { get; set; }
+        public string Content { get; set; }
     }
 }
