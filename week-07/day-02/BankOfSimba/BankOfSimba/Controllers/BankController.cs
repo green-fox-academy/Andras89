8b1f0e487980620ec2b0e6c212742eb297c0e81e
Andras <palfi.andras89@gmail.com>
2018-06-26 21:39:27 +0200

    refactor(BankOfSimba): animal names

--- week-07/day-02/BankOfSimba/BankOfSimba/Controllers/BankController.cs
+++ week-07/day-02/BankOfSimba/BankOfSimba/Controllers/BankController.cs
@@ -15,9 +15,9 @@ namespace BankOfSimba.Controllers
                 {
                     new BankAccount(){ Name="Simba", Balance=5000, AnimalType="Lion", IsKing=true, IsGoodGuy=true },
                     new BankAccount(){ Name="Mufasa", Balance=32000, AnimalType="Lion", IsKing=true, IsGoodGuy=true },
-                    new BankAccount(){ Name="Zazu", Balance=3000, AnimalType="Bird", IsKing=false, IsGoodGuy=true },
+                    new BankAccount(){ Name="Zazu", Balance=3000, AnimalType="Hornbill", IsKing=false, IsGoodGuy=true },
                     new BankAccount(){ Name="Timon", Balance=20, AnimalType="Meerkat", IsKing=false, IsGoodGuy=true },
-                    new BankAccount(){ Name="Pumba", Balance=0, AnimalType="Boar", IsKing=false, IsGoodGuy=true },
+                    new BankAccount(){ Name="Pumba", Balance=0, AnimalType="Warthog", IsKing=false, IsGoodGuy=true },
                     new BankAccount(){ Name="Scar", Balance=400000, AnimalType="Lion", IsKing=true, IsGoodGuy=false }
                 }
             };
