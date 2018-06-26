a0acf87abd9044b18a7d789cb978c4aa03e4849d
Andras <palfi.andras89@gmail.com>
2018-06-26 13:17:19 +0200

    add(BankOfSimba): BankAccountInfoViewModel.cshtml

--- week-07/day-02/BankOfSimba/BankOfSimba/Controllers/BankController.cs
+++ week-07/day-02/BankOfSimba/BankOfSimba/Controllers/BankController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BankOfSimba.Models;
+using BankOfSimba.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BankOfSimba.Controllers
@@ -13,9 +14,27 @@ namespace BankOfSimba.Controllers
         [HttpGet("Index")]
         public IActionResult Index()
         {
-            BankAccount account = new BankAccount("Simba", 2000, "Animal.Lion");            
+            BankAccount account = new BankAccount("Simba", 2000, "Lion");
 
             return View(account);
+        }
+
+        [HttpGet("AccountInfo")]
+        public IActionResult AccountInfo()
+        {
+            BankAccountInfoViewModel bankDatabase = new BankAccountInfoViewModel()
+            {
+                BankAccountDatabase = new List<BankAccount>
+                {
+                    new BankAccount("Simba", 5000, "Lion"),
+                    new BankAccount("Musafa", 32000, "Lion"),
+                    new BankAccount("Zazu", 3000, "Bird"),
+                    new BankAccount("Timon", 20, "Meerkat"),
+                    new BankAccount("Pumba", 0, "Boar")
+                }
+            };
+
+            return View(bankDatabase);
         }
     }
 }
---
+++ week-07/day-02/BankOfSimba/BankOfSimba/ViewModels/BankAccountInfoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BankOfSimba.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.ViewModels
+{
+    public class BankAccountInfoViewModel
+    {
+        public List<BankAccount> BankAccountDatabase { get; set; }
+    }
+}
