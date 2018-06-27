b744801e8714b87497fc16106e954910ce6c4ce2
Andras <palfi.andras89@gmail.com>
2018-06-27 14:45:43 +0200

    feat(ToDoApp): DI

--- week-07/day-03/ToDoApp/ToDoApp/Controllers/ToDoController.cs
+++ week-07/day-03/ToDoApp/ToDoApp/Controllers/ToDoController.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Services;
 
 namespace ToDoApp.Controllers
 {
     [Route("/")]
     public class ToDoController : Controller
     {
+        IToDo database;
+
+        public ToDoController(IToDo database)
+        {
+            this.database = database;
+        }
+
         [HttpGet("/")]
         public IActionResult Index()
         {
--- week-07/day-03/ToDoApp/ToDoApp/Startup.cs
+++ week-07/day-03/ToDoApp/ToDoApp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ToDoApp.Services;
 
 namespace ToDoApp
 {
@@ -22,6 +23,7 @@ namespace ToDoApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<IToDo, ToDoService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
