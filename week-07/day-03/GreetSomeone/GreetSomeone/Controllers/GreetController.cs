b686605feb77a3f62f5116e1af149a6453c4ddaf
Andras <palfi.andras89@gmail.com>
2018-06-27 13:10:38 +0200

    chore(GreetSomeone)

---
+++ week-07/day-03/GreetSomeone/GreetSomeone/Controllers/GreetController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreetSomeone.Controllers
+{
+    [Route("/")]
+    public class GreetController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/greet")]
+        public IActionResult Greet()
+        {
+            return View();
+        }
+
+        [HttpPost("/")]
+        public IActionResult GetName()
+        {
+            return RedirectToAction("Greet");
+        }
+    }
+}
