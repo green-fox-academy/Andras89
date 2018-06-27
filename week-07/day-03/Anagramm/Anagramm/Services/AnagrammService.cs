8d557cea8d2c260c79ea5f93cd0ae214b23aaff5
Andras <palfi.andras89@gmail.com>
2018-06-27 14:02:16 +0200

    add(Anagramm): Services

---
+++ week-07/day-03/Anagramm/Anagramm/Services/AnagrammService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anagramm.Services
+{
+    public class AnagrammService : IAnagramm
+    {
+        private string anagramm;
+
+        public bool CheckAnagramm(string input)
+        {
+            var temp = input.ToCharArray();
+            Array.Reverse(temp);
+            anagramm = string.Join("", temp);
+
+            return input.ToLower().Equals(anagramm.ToLower());
+        }
+    }
+}
---
+++ week-07/day-03/Anagramm/Anagramm/Services/IAnagramm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Anagramm.Services
+{
+    public interface IAnagramm
+    {
+        string GetAnagramm();
+        bool CheckAnagramm();
+    }
+}
