9f741a52ee69a161d6ef23122401438e807cf4e3
Andras <palfi.andras89@gmail.com>
2018-07-01 14:06:46 +0200

    feat(QuerySyntax class): NumberFreq()

--- week-08/day-01/LINQPractice/LINQPractice/QuerySyntax.cs
+++ week-08/day-01/LINQPractice/LINQPractice/QuerySyntax.cs
@@ -10,6 +10,20 @@ namespace LINQPractice
     {
         public List<int> InternalIntStructure { get; set; }
 
+        public Dictionary<int,int> NumFrequency()
+        {
+            Dictionary<int, int> freq = (from current in InternalIntStructure
+                                        group current by current into diffnumber
+                                        select new
+                                        {
+                                            HeadNumber = diffnumber.Key,
+                                            HeadCount = diffnumber.Count()
+                                        })
+                                        .ToDictionary(x => x.HeadCount, x => x.HeadCount);
+
+            return freq;
+        }
+
         public List<int> Square()
         {
             List<int> square = (from current in InternalIntStructure
