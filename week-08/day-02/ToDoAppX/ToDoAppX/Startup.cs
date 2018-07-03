34c92a1486aa1fa005c119275bbd5f07ecbf63a0
Andras <palfi.andras89@gmail.com>
2018-07-03 14:30:51 +0200

    add(View): List.cshtml

--- week-08/day-02/ToDoAppX/ToDoAppX/Startup.cs
+++ week-08/day-02/ToDoAppX/ToDoAppX/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ToDoAppX.Repositories;
 
 namespace ToDoAppX
 {
@@ -22,6 +23,7 @@ namespace ToDoAppX
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddTransient<ICRUD, ToDoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
