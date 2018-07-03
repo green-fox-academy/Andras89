5ed8ced8075bb75fb2a911852842943b92f91667
Andras <palfi.andras89@gmail.com>
2018-07-03 13:22:37 +0200

    chore(ToDoAppX)

---
+++ week-08/day-02/ToDoAppX/ToDoAppX/Models/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoAppX.Models
+{
+    public class ToDo
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public bool IsUrgent { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
