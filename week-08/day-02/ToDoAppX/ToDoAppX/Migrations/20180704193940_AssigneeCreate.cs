13f5af7de53574344f1040525a5c54e5934deeee
Andras <palfi.andras89@gmail.com>
2018-07-04 21:49:19 +0200

    feat(Assignee class): db migration

---
+++ week-08/day-02/ToDoAppX/ToDoAppX/Migrations/20180704193940_AssigneeCreate.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoAppX.Migrations
+{
+    public partial class AssigneeCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AssigneeId",
+                table: "Todos",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Assignees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assignees", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Todos_AssigneeId",
+                table: "Todos",
+                column: "AssigneeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Todos_Assignees_AssigneeId",
+                table: "Todos",
+                column: "AssigneeId",
+                principalTable: "Assignees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Todos_Assignees_AssigneeId",
+                table: "Todos");
+
+            migrationBuilder.DropTable(
+                name: "Assignees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Todos_AssigneeId",
+                table: "Todos");
+
+            migrationBuilder.DropColumn(
+                name: "AssigneeId",
+                table: "Todos");
+        }
+    }
+}
