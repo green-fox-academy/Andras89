51bbadf44e3d26fe4cd9224917dc3ecb8bb686a3
Andras <palfi.andras89@gmail.com>
2018-07-04 15:13:09 +0200

    refactor(FoxRepository)

--- week-08/day-02/FoxClub/FoxClub/Repositories/FoxRepository.cs
+++ week-08/day-02/FoxClub/FoxClub/Repositories/FoxRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FoxClub.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +9,34 @@ namespace FoxClub.Repositories
 {
     public class FoxRepository : IFoxRepository
     {
-        DbContext database;
+        FoxDbContext database;
 
-        public FoxRepository(DbContext database)
+        public FoxRepository(FoxDbContext database)
         {
             this.database = database;
+        }
+
+        public void Create(Fox fox)
+        {
+            database.Add(fox);
+            database.SaveChanges();
+        }
+
+        public void Delete(Fox fox)
+        {
+            database.Remove(fox);
+            database.SaveChanges();
+        }
+
+        public Fox Read(long id)
+        {
+            var output = database.Foxes.ToList().FirstOrDefault(f => f.Id == id);
+            return output;
+        }
+
+        public void Update(Fox fox)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- week-08/day-02/FoxClub/FoxClub/Repositories/IFoxRepository.cs
+++ week-08/day-02/FoxClub/FoxClub/Repositories/IFoxRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoxClub.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace FoxClub.Repositories
 {
     public interface IFoxRepository
     {
+        void Create(Fox fox);
+        Fox Read(long id);
+        void Update(Fox fox);
+        void Delete(Fox fox);
     }
 }
