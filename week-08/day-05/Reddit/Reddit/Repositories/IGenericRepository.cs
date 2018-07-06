b8389dc24fc9a7994b5c37256008b65685f9a3b4
Andras <palfi.andras89@gmail.com>
2018-07-06 10:46:17 +0200

    add(IGenericRepository)

---
+++ week-08/day-05/Reddit/Reddit/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Repositories
+{
+    public interface IGenericRepository<Type>
+    {
+        void Add(Type element);
+        List<Type> Read();
+        void Update(Type element);
+        void Delete(Type element);
+    }
+}
