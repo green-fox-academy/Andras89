e3bb7e18e9244aaa8c3757c16705304ea0eddd9d
Andras <palfi.andras89@gmail.com>
2018-07-06 10:35:33 +0200

    add(RedditDbContext)

---
+++ week-08/day-05/Reddit/Reddit/Models/RedditDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Models
+{
+    public class RedditDbContext : DbContext
+    {
+        private DbSet<Post> Posts { get; set; }
+
+        public RedditDbContext(DbContextOptions<RedditDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
