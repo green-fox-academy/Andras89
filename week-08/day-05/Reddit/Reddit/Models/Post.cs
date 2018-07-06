f9caab664ae0fb2f8b741321a83cfbeca7af4621
Andras <palfi.andras89@gmail.com>
2018-07-06 10:33:03 +0200

    add(Post class)

---
+++ week-08/day-05/Reddit/Reddit/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public int Vote { get; set; }
+    }
+}
