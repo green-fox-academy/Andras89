762ecd19a01dd3fba2a814557e85f8094bf9d9c8
Andras <palfi.andras89@gmail.com>
2018-07-06 10:48:37 +0200

    add(PostRepository)

---
+++ week-08/day-05/Reddit/Reddit/Repositories/PostRepository.cs
@@ -0,0 +1,38 @@
+﻿using Reddit.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reddit.Repositories
+{
+    public class PostRepository : IGenericRepository<Post>
+    {
+        RedditDbContext dbContext;
+
+        public PostRepository(RedditDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Add(Post element)
+        {
+            
+        }
+
+        public void Delete(Post element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Post> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Post element)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
