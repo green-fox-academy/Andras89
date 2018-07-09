f0b4b6591ce53c8cc4e184a19d39cad6e0ecbcab
Andras <palfi.andras89@gmail.com>
2018-07-09 16:23:36 +0200

    feat(LoggerRepository): Methods

--- week-09/day-01/Frontend/Frontend/Repositories/IGenericRepository.cs
+++ week-09/day-01/Frontend/Frontend/Repositories/IGenericRepository.cs
@@ -5,8 +5,9 @@ using System.Threading.Tasks;
 
 namespace Frontend.Repository
 {
-    public interface IGenericRepository
+    public interface IGenericRepository<Type>
     {
-
+        void Create(Type element);
+        List<Type> Read();
     }
 }
