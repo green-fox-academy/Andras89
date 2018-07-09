28ed055d2b55058903380b1587c29bb3fb3cd276
Andras <palfi.andras89@gmail.com>
2018-07-09 13:46:47 +0200

    feat(HomeController): Greeter()

--- week-09/day-01/Frontend/Frontend/Controllers/HomeController.cs
+++ week-09/day-01/Frontend/Frontend/Controllers/HomeController.cs
@@ -16,5 +16,33 @@ namespace Frontend.Controllers
         {
             return File("index.html", "text/html");
         }
+
+        [HttpGet("doubling")]
+        public IActionResult Doubling([FromQuery] int? input)
+        {
+            if(input is null)
+            {
+                return Json(new { error = "Please provide an input!" });
+            }
+            else
+            {
+                return Json(new {
+                                    received = input,
+                                    result = input * 2});
+            }
+        }
+
+        [HttpGet("greeter")]
+        public IActionResult Greeter([FromQuery] string name, [FromQuery] string title)
+        {
+            if(name is null || title is null)
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+            else
+            {
+                return Json(new { welcome_message = $"Oh, hi there {name}, my dear {title}"});
+            }
+        }
     }
 }
