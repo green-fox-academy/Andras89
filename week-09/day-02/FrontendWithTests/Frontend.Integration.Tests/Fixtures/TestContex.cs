e8b4abcefa321d7185e8a0b26710a8cef6766f2f
Andras <palfi.andras89@gmail.com>
2018-07-09 21:34:19 +0200

    add(Frontend.Integration.Test)

---
+++ week-09/day-02/FrontendWithTests/Frontend.Integration.Tests/Fixtures/TestContex.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Frontend.Integration.Tests.Fixtures
+{
+    public class TestContex
+    {
+        private TestServer server;
+
+        public HttpClient Client { get; set; }
+
+        public TestContex()
+        {
+            server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client = server.CreateClient();
+        }
+    }
+}
---
+++ week-09/day-02/FrontendWithTests/Frontend.Integration.Tests/Scenarios/HomeControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using Frontend.Integration.Tests.Fixtures;
+using Newtonsoft.Json;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Frontend.Integration.Tests.Scenarios
+{
+    public class HomeControllerTests
+    {
+        private readonly TestContex testFixture;
+
+        public HomeControllerTests()
+        {
+            testFixture = new TestContex();
+        }
+
+        [Fact]
+        public async Task DoublingReturnOkResponse()
+        {
+            var response = await testFixture.Client.GetAsync("doubling?input=3");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task DoublingReturnJsonResponse()
+        {
+            var response = await testFixture.Client.GetAsync("doubling?input=3");
+
+            Assert.Equal(JsonConvert.SerializeObject(new { received = 3, result = 6 }).ToLower(),
+                         response.Content.ReadAsStringAsync().Result.ToLower());
+        }
+    }
+}
