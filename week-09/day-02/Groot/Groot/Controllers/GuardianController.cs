350e4f5b3df2b7fe9e2a569e78c49c4505027875
Andras <palfi.andras89@gmail.com>
2018-07-10 13:59:10 +0200

    chore(Groot.Integration.Tests)

---
+++ week-09/day-02/Groot/Groot/Controllers/GuardianController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Groot.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Guardian")]
+    public class GuardianController : Controller
+    {
+        [HttpGet("groot")]
+        public IActionResult Groot([FromQuery]string message)
+        {
+            if(message is null)
+            {
+                return Json(new { error = "I am Groot!" });
+            }
+            else
+            {
+                return Json(new { received = message, translated = "I am Groot!" });
+            }
+        }
+    }
+}
