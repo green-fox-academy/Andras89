51a59cc26d26d9d39973b75bee01020de8822459
Andras <palfi.andras89@gmail.com>
2018-07-11 19:16:01 +0200

    feat(WarehouseApp): add database , scaffold

---
+++ week-09/day-03/WarehouseApp/WarehouseApp/Models/GreenFoxWarehouseContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WarehouseApp.Models
+{
+    public partial class GreenFoxWarehouseContext : DbContext
+    {
+        public GreenFoxWarehouseContext()
+        {
+        }
+
+        public GreenFoxWarehouseContext(DbContextOptions<GreenFoxWarehouseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Warehouse> Warehouse { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=GreenFoxWarehouse;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Warehouse>(entity =>
+            {
+                entity.ToTable("warehouse");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Category)
+                    .HasColumnName("category")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ItemName)
+                    .HasColumnName("item_name")
+                    .HasMaxLength(80)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Manufacturer)
+                    .HasColumnName("manufacturer")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Size)
+                    .HasColumnName("size")
+                    .HasMaxLength(4)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UnitPrice).HasColumnName("unit_price");
+            });
+        }
+    }
+}
---
+++ week-09/day-03/WarehouseApp/WarehouseApp/Models/Warehouse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WarehouseApp.Models
+{
+    public partial class Warehouse
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public string Manufacturer { get; set; }
+        public string Category { get; set; }
+        public string Size { get; set; }
+        public double? UnitPrice { get; set; }
+    }
+}
