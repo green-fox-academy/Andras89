e147e1a1d93c3b82eb39360c54d5cd1dea05f0b1
Andras <palfi.andras89@gmail.com>
2018-07-11 11:03:24 +0200

    feat(RedditRest): PostRepo

---
+++ week-09/day-03/RedditREST/RedditREST/Repositories/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedditREST.Repositories
+{
+    public interface IGenericRepository<Type>
+    {
+        void Create(Type element);
+        List<Type> Read();
+        void Update(Type element);
+        void Delete(Type element);
+    }
+}
---
+++ week-09/day-03/RedditREST/RedditREST/Repositories/PostRepository.cs
@@ -0,0 +1,41 @@
+﻿using RedditREST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedditREST.Repositories
+{
+    public class PostRepository : IGenericRepository<Post>
+    {
+        private RedditRestDbContext redditRestDb;
+
+        public PostRepository(RedditRestDbContext redditRestDb)
+        {
+            this.redditRestDb = redditRestDb;
+        }
+
+        public void Create(Post element)
+        {
+            redditRestDb.Posts.Add(element);
+            redditRestDb.SaveChanges();
+        }
+
+        public void Delete(Post element)
+        {
+            redditRestDb.Posts.Remove(element);
+            redditRestDb.SaveChanges();
+        }
+
+        public List<Post> Read()
+        {
+            return redditRestDb.Posts.ToList();
+        }
+
+        public void Update(Post element)
+        {
+            redditRestDb.Posts.Update(element);
+            redditRestDb.SaveChanges();
+        }
+    }
+}
