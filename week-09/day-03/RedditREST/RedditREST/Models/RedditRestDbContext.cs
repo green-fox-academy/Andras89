6146468ad90b51bc1cb6dad215587e4a588bb92c
Andras <palfi.andras89@gmail.com>
2018-07-11 10:21:18 +0200

    add(RedditRESTDbContext)

---
+++ week-09/day-03/RedditREST/RedditREST/Models/RedditRestDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedditREST.Models
+{
+    public class RedditRestDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        public RedditRestDbContext(DbContextOptions<RedditRestDbContext> options) : base(options)
+        {
+        }
+    }
+}
