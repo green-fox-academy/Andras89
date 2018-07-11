c77b0ddab6d8ec904925bfd3b4856015c9d0a788
Andras <palfi.andras89@gmail.com>
2018-07-11 19:24:34 +0200

    add(WarehouseApp): WarehouseRepo, Iservice

---
+++ week-09/day-03/WarehouseApp/WarehouseApp/Repositories/WarehouseRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WarehouseApp.Models;
+
+namespace WarehouseApp.Repositories
+{
+    public class WarehouseRepository : IGenericRepository<Warehouse>
+    {
+        GreenFoxWarehouseContext databaseContext;
+
+        public WarehouseRepository(GreenFoxWarehouseContext databaseContext)
+        {
+            this.databaseContext = databaseContext;
+        }
+
+        public void Create(Warehouse element)
+        {
+            databaseContext.Warehouse.Add(element);
+            databaseContext.SaveChanges();
+        }
+
+        public void Delete(Warehouse element)
+        {
+            databaseContext.Warehouse.Remove(element);
+            databaseContext.SaveChanges();
+        }
+
+        public List<Warehouse> Read()
+        {
+            return databaseContext.Warehouse.ToList();
+        }
+
+        public void Update(Warehouse element)
+        {
+            databaseContext.Warehouse.Update(element);
+            databaseContext.SaveChanges();
+        }
+    }
+}
