6a93739160ea05a12c0be29f6898704af16c3194
Andras <palfi.andras89@gmail.com>
2018-07-11 10:17:09 +0200

    add(Post class)

---
+++ week-09/day-03/RedditREST/RedditREST/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedditREST.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public double Timestamp { get; set; }
+        public int Score { get; set; }
+    }
+}
