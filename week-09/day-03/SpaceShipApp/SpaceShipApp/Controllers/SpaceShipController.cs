9185a1640d4d1bfcdb0299c76d039ef38dd844ed
Andras <palfi.andras89@gmail.com>
2018-07-12 14:25:46 +0200

    feat(SpaceTable.cshtml): much table happiness yolo

--- week-09/day-03/SpaceShipApp/SpaceShipApp/Controllers/SpaceShipController.cs
+++ week-09/day-03/SpaceShipApp/SpaceShipApp/Controllers/SpaceShipController.cs
@@ -20,7 +20,7 @@ namespace SpaceShipApp.Controllers
         [HttpGet("")]
         public IActionResult SpaceTable()
         {
-            return View();
+            return View(spaceService.GetAllSpaceEntities());
         }
     }
 }
