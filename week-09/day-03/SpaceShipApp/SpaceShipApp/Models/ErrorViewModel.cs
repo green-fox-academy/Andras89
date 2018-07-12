3972a1e9845442503c8761a8ca77695e1531c6bd
Andras <palfi.andras89@gmail.com>
2018-07-12 13:43:34 +0200

    chore(SpaceShipApp)

---
+++ week-09/day-03/SpaceShipApp/SpaceShipApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpaceShipApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
