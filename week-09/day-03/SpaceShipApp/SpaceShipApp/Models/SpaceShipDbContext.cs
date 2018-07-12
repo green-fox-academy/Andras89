67bebae2acdab9c4049d6a76c9d65a555a33423a
Andras <palfi.andras89@gmail.com>
2018-07-12 13:51:38 +0200

    add(DbMigration)

---
+++ week-09/day-03/SpaceShipApp/SpaceShipApp/Models/SpaceShipDbContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SpaceShipApp.Models
+{
+    public partial class SpaceShipDbContext : DbContext
+    {
+        public SpaceShipDbContext()
+        {
+        }
+
+        public SpaceShipDbContext(DbContextOptions<SpaceShipDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Planet> Planet { get; set; }
+        public virtual DbSet<Spaceship> Spaceship { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SpaceShipDb;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Planet>(entity =>
+            {
+                entity.ToTable("planet");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Population).HasColumnName("population");
+            });
+
+            modelBuilder.Entity<Spaceship>(entity =>
+            {
+                entity.ToTable("spaceship");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.MaxCapacity).HasColumnName("max_capacity");
+
+                entity.Property(e => e.Planet)
+                    .HasColumnName("planet")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Utilization).HasColumnName("utilization");
+            });
+        }
+    }
+}
---
+++ week-09/day-03/SpaceShipApp/SpaceShipApp/Models/Spaceship.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaceShipApp.Models
+{
+    public partial class Spaceship
+    {
+        public int Id { get; set; }
+        public int MaxCapacity { get; set; }
+        public string Planet { get; set; }
+        public int Utilization { get; set; }
+    }
+}
