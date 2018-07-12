763b8904deabb4712401841abab28dc58e329a71
Andras <palfi.andras89@gmail.com>
2018-07-12 14:04:39 +0200

    add(IService, SpaceShipService)

---
+++ week-09/day-03/SpaceShipApp/SpaceShipApp/Services/SpaceShipAppService.cs
@@ -0,0 +1,22 @@
+﻿using SpaceShipApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceShipApp.Services
+{
+    public class SpaceShipAppService : IService
+    {
+        private SpaceshipRepository spaceShipRepo;
+        private PlanetRepository planetRepo;
+
+        public SpaceShipAppService(SpaceshipRepository spaceShipRepo, PlanetRepository planetRepo)
+        {
+            this.spaceShipRepo = spaceShipRepo;
+            this.planetRepo = planetRepo;
+        }
+
+
+    }
+}
--- week-09/day-03/SpaceShipApp/SpaceShipApp/Startup.cs
+++ week-09/day-03/SpaceShipApp/SpaceShipApp/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SpaceShipApp.Models;
 
 namespace SpaceShipApp
 {
@@ -22,6 +23,7 @@ namespace SpaceShipApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddTransient<SpaceShipDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
