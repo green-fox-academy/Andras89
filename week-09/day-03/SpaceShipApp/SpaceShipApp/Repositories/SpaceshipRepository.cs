d7a7e58216ae8f2df4de7ed927677d807f04a641
Andras <palfi.andras89@gmail.com>
2018-07-12 14:01:22 +0200

    feat(SpaceShipRepo): CRUD methods

--- week-09/day-03/SpaceShipApp/SpaceShipApp/Repositories/SpaceshipRepository.cs
+++ week-09/day-03/SpaceShipApp/SpaceShipApp/Repositories/SpaceshipRepository.cs
@@ -1,11 +1,41 @@
-﻿using System;
+﻿using SpaceShipApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SpaceShipApp.Repositories
 {
-    public class SpaceshipRepository
+    public class SpaceshipRepository : IGenericRepository<Spaceship>
     {
+        private SpaceShipDbContext dbContext;
+
+        public SpaceshipRepository(SpaceShipDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Create(Spaceship element)
+        {
+            dbContext.Spaceship.Add(element);
+            dbContext.SaveChanges();
+        }
+
+        public void Delete(Spaceship element)
+        {
+            dbContext.Spaceship.Remove(element);
+            dbContext.SaveChanges();
+        }
+
+        public List<Spaceship> Read()
+        {
+            return dbContext.Spaceship.ToList();
+        }
+
+        public void Update(Spaceship element)
+        {
+            dbContext.Spaceship.Update(element);
+            dbContext.SaveChanges();
+        }
     }
 }
