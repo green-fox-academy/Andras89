626e9b2a9d6a887030c0fcd64f52a1f182a6b217
Andras <palfi.andras89@gmail.com>
2018-07-16 21:13:58 +0200

    add(Practice): StringFunctions

---
+++ Python/Practice/StringFunctions.py
@@ -0,0 +1,28 @@
+practiceString = 'Hello World!'
+
+print(practiceString.capitalize())
+
+print(practiceString.swapcase())
+
+print(len(practiceString))
+
+print(practiceString.replace('World', 'Everyone'))
+
+sub = 'l'
+print(practiceString.count(sub))
+
+print(practiceString.startswith('Hello'))
+
+print(practiceString.endswith('!'))
+
+print(practiceString.split())
+
+print(practiceString.find('e'))
+
+print(practiceString.index("o"))
+
+print(practiceString.isalnum())
+
+print(practiceString.isalpha())
+
+print(practiceString.isnumeric())
