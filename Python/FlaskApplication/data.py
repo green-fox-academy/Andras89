aa555382c907d0c0c2a5c9a6dc5ba3bdeb08f5ab
Andras <palfi.andras89@gmail.com>
2018-07-17 19:30:03 +0200

    add(data.py)

---
+++ Python/FlaskApplication/data.py
@@ -0,0 +1,25 @@
+def all_articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum',
+            'author': 'Andrew',
+            'created at': '17-07-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum',
+            'author': 'Andrew',
+            'created at': '17-07-2018'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum',
+            'author': 'Andrew',
+            'created at': '17-07-2018'
+        }
+    ]
+    return articles
