c3c4c688512d2e170e2b4bec799a0715d27d1392
Andras <palfi.andras89@gmail.com>
2018-07-18 20:37:34 +0200

    feat(FlaskApplication): mysql import

--- Python/FlaskApplication/FlaskApp.py
+++ Python/FlaskApplication/FlaskApp.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
 from data import all_articles
+import PyMySQLdb
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 application = Flask(__name__)
 
