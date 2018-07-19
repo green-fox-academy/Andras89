1144b93f7258640281adcbe9f34b22cafeef28d1
Andras <palfi.andras89@gmail.com>
2018-07-19 20:19:11 +0200

    add(checkerboard)

--- Java/src/com/company/Main.java
+++ Java/src/com/company/Main.java
@@ -22,5 +22,12 @@ public class Main {
         purpleFrame.add(drawSteps);
         purpleFrame.setSize(800, 800);
         purpleFrame.setVisible(true);
+
+        JFrame checkerFrame = new JFrame("Checker Board");
+        checkerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        CheckerBoard cBoard = new CheckerBoard(800);
+        checkerFrame.add(cBoard);
+        checkerFrame.setSize(800, 800);
+        checkerFrame.setVisible(true);
     }
 }
