405baf44d62de363254faff326ccaadcd60765a3
Andras <palfi.andras89@gmail.com>
2018-07-19 19:16:51 +0200

    feat(Draw.java): Rectangles

--- Java/src/com/company/Main.java
+++ Java/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
+        JFrame frame = new JFrame("Drawing Board");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Draw drawing = new Draw();
+        frame.add(drawing);
+        frame.setSize(600, 600);
+        frame.setVisible(true);
     }
 }
