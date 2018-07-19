3f93fcfaa144adf1c9fc13ae195d8d27ca044df3
Andras <palfi.andras89@gmail.com>
2018-07-19 21:11:43 +0200

    add(People)

---
+++ Java/StreamPractice/src/com/company/People.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class People
+{
+    public String firstName;
+    public String lastName;
+    public int age;
+
+    public People(String firstName, String lastName, int age)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
