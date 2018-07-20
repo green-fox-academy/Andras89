df0c61de8c76a70cfe45796eb294a7d995a54eda
Andras <palfi.andras89@gmail.com>
2018-07-20 20:46:05 +0200

    chore(NewGradle)

---
+++ Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/GradleProjectApp.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleProjectApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(GradleProjectApp.class, args);
+    }
+}
