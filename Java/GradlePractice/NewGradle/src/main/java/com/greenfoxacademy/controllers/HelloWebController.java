c8f39a456a104c5f1e7c7c07c421d7ef5e7f8325
Andras <palfi.andras89@gmail.com>
2018-07-21 17:39:23 +0200

    add(HelloWorldWebController)

--- Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/controllers/HelloWebController.java
+++ Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/controllers/HelloWebController.java
@@ -1,15 +1,19 @@
 package com.greenfoxacademy.controllers;
 
+import com.greenfoxacademy.models.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWebController {
 
     @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World");
+    public String greeting(Model model, @RequestParam(value = "name") String name) {
+        Greeting greeting = new Greeting(name);
+        model.addAttribute("name", greeting.getContent());
+        model.addAttribute("id", greeting.getId());
         return "greeting";
     }
 }
