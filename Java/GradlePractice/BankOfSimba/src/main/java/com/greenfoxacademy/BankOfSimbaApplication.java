ffea9dd0ec9dd4e24d98e2c1a5706fbdb00288d9
Andras <palfi.andras89@gmail.com>
2018-07-21 20:16:07 +0200

    chore(BankOfSimba)

---
+++ Java/GradlePractice/BankOfSimba/src/main/java/com/greenfoxacademy/BankOfSimbaApplication.java
@@ -0,0 +1,15 @@
+package com.greenfoxacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class BankOfSimbaApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(BankOfSimbaApplication.class, args);
+    }
+}
