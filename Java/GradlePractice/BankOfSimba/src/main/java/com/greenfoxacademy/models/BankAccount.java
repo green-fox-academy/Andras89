efb1a8e469ad0a2f3857187bf061a90e4cacfefe
Andras <palfi.andras89@gmail.com>
2018-07-21 20:26:41 +0200

    add(BankAccount)

---
+++ Java/GradlePractice/BankOfSimba/src/main/java/com/greenfoxacademy/models/BankAccount.java
@@ -0,0 +1,38 @@
+package com.greenfoxacademy.models;
+
+public class BankAccount {
+
+    private String name;
+    private double balance;
+    private String animalType;
+
+    public BankAccount(String name, double balance, String animalType) {
+        this.name = name;
+        this.balance = balance;
+        this.animalType = animalType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+}
