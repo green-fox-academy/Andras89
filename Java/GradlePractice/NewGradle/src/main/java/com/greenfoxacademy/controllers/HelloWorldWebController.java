0b598256de388d9458256082ed7d2dc2ca5bf16d
Andras <palfi.andras89@gmail.com>
2018-07-21 18:24:13 +0200

    feat(MultiLang): Methods

--- Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/controllers/HelloWorldWebController.java
+++ Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/controllers/HelloWorldWebController.java
@@ -9,11 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloWorldWebController {
 
-    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
-            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
-            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
-            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
-
     @RequestMapping("/web/multigreet")
     public String multiLangGreeting(Model model){
         model.addAttribute("hellos", hellos);
--- Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/models/MultiLang.java
+++ Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/models/MultiLang.java
@@ -2,5 +2,29 @@ package com.greenfoxacademy.models;
 
 public class MultiLang {
 
-    
+    private String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
+    public String getHello(){
+        int chosenIndex = (int) (Math.random() * hellos.length);
+        return hellos[chosenIndex];
+    }
+
+    public int getRed(){
+        return (int) (Math.random() * 256);
+    }
+
+    public int getGreen(){
+        return (int) (Math.random() * 256);
+    }
+
+    public int getBlue(){
+        return (int) (Math.random() * 256);
+    }
+
+    public int getFont(){
+        return (int) (Math.random() * 60 + 8);
+    }
 }
