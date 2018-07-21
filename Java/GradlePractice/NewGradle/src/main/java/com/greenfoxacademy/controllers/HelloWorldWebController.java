e02780ebce75ecb2fd49e101f8bb51082d5da6e6
Andras <palfi.andras89@gmail.com>
2018-07-21 18:28:41 +0200

    feat(HelloWorldWebController): multiLangGreet()

--- Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/controllers/HelloWorldWebController.java
+++ Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/controllers/HelloWorldWebController.java
@@ -1,17 +1,24 @@
 package com.greenfoxacademy.controllers;
 
+import com.greenfoxacademy.models.MultiLang;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldWebController {
 
     @RequestMapping("/web/multigreet")
     public String multiLangGreeting(Model model){
-        model.addAttribute("hellos", hellos);
+        MultiLang multi = new MultiLang();
+
+        model.addAttribute("hello", multi.getHello());
+        model.addAttribute("red", multi.getRed());
+        model.addAttribute("green", multi.getGreen());
+        model.addAttribute("blue", multi.getBlue());
+        model.addAttribute("font", multi.getFont());
+
         return "multigreet";
     }
 }
