4a35606b491bb34506ec0386a8fdcbf9bbdc39fb
Andras <palfi.andras89@gmail.com>
2018-07-21 16:27:53 +0200

    feat(HelloRESTController): greeting()

--- Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/controllers/HelloRESTController.java
+++ Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/controllers/HelloRESTController.java
@@ -1,9 +1,16 @@
 package com.greenfoxacademy.controllers;
 
+import com.greenfoxacademy.models.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
-    
+    Greeting greet = new Greeting(1, "Yo!");
+
+    @RequestMapping
+    public Greeting greeting(){
+        return greet;
+    }
 }
