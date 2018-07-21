f9f9e0238ba3c4dde7a075211c3d073447a5df99
Andras <palfi.andras89@gmail.com>
2018-07-21 20:34:56 +0200

    feat(BankController): Service DI

--- Java/GradlePractice/BankOfSimba/src/main/java/com/greenfoxacademy/controllers/BankController.java
+++ Java/GradlePractice/BankOfSimba/src/main/java/com/greenfoxacademy/controllers/BankController.java
@@ -1,8 +1,17 @@
 package com.greenfoxacademy.controllers;
 
+import com.greenfoxacademy.services.BankService;
+import com.greenfoxacademy.services.BankServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class BankController {
 
+    private BankService bankAccService;
+
+    @Autowired
+    public BankController(BankServiceImpl bankAccService) {
+        this.bankAccService = bankAccService;
+    }
 }
