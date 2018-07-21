cf4c05546006d903c6d83fcc658e97f4863735ef
Andras <palfi.andras89@gmail.com>
2018-07-21 15:11:10 +0200

    feat(HelloController): Hello()

--- Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/controllers/HelloController.java
+++ Java/GradlePractice/NewGradle/src/main/java/com/greenfoxacademy/controllers/HelloController.java
@@ -1,7 +1,15 @@
 package com.greenfoxacademy.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    @ResponseBody
+    public String Hello(){
+        return "Hello World";
+    }
 }
