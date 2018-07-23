b5fb2e342bc6d6305e674a3f7a567f0e71563e26
Andras <palfi.andras89@gmail.com>
2018-07-23 17:23:29 +0200

    feat(RedditController, RedditServiceImpl): DI

--- Java/GradlePractice/RedditCloneApp/src/main/java/com/greenfoxacademy/controllers/RedditController.java
+++ Java/GradlePractice/RedditCloneApp/src/main/java/com/greenfoxacademy/controllers/RedditController.java
@@ -1,10 +1,15 @@
 package com.greenfoxacademy.controllers;
 
+import com.greenfoxacademy.services.RedditService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RedditController {
+
+    @Autowired
+    private RedditService service;
 
     @GetMapping("")
     public String MainPage(){
--- Java/GradlePractice/RedditCloneApp/src/main/java/com/greenfoxacademy/services/RedditServiceImpl.java
+++ Java/GradlePractice/RedditCloneApp/src/main/java/com/greenfoxacademy/services/RedditServiceImpl.java
@@ -1,4 +1,12 @@
 package com.greenfoxacademy.services;
 
+import com.greenfoxacademy.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RedditServiceImpl implements RedditService {
+
+    @Autowired
+    private PostRepository postRepo;
 }
