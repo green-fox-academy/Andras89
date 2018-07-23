e32a0c36eb741ff8257bcb1705b9813addc02f22
Andras <palfi.andras89@gmail.com>
2018-07-23 17:15:50 +0200

    feat(Post): fields

--- Java/GradlePractice/RedditCloneApp/src/main/java/com/greenfoxacademy/models/Post.java
+++ Java/GradlePractice/RedditCloneApp/src/main/java/com/greenfoxacademy/models/Post.java
@@ -1,4 +1,17 @@
 package com.greenfoxacademy.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String content;
+    private String url;
+    private int votes;
 }
