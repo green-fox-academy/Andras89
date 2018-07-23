16cd295cdc2c9f4438cbdab6cc77d737b497f161
Andras <palfi.andras89@gmail.com>
2018-07-23 22:58:40 +0200

    refactor(circles): create defs

--- Python/Drawing/circles.py
+++ Python/Drawing/circles.py
@@ -10,28 +10,32 @@ outer = 4
 pos = 8
 cicle = 16
 
-for u in range(outer):
+
+def starting():
     draw.penup()
     draw.forward(60)
     draw.pendown()
 
-    for j in range(pos):
-        draw.penup()
-        draw.forward(60)
-        draw.pendown()
 
-        for i in range(cicle):
-            draw.circle(120)
-            draw.right(360/cicle)
-
-        draw.penup()
-        draw.backward(60)
-        draw.pendown()
-        draw.left(360/pos)
-
+def ending(amount):
     draw.penup()
     draw.backward(60)
     draw.pendown()
-    draw.left(360 / outer)
+    draw.left(360 / amount)
+
+
+for u in range(outer):
+    starting()
+
+    for j in range(pos):
+        starting()
+
+        for i in range(cicle):
+            draw.circle(120)
+            draw.right(360 / cicle)
+
+        ending(pos)
+
+    ending(outer)
 
 tkinter.mainloop()
