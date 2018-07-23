eba62cefff0e90ee840dc88097ab3ed6c904c5e5
Andras <palfi.andras89@gmail.com>
2018-07-23 23:43:55 +0200

    feat(landscape.py): map tile draw

--- Python/Drawing/landscape.py
+++ Python/Drawing/landscape.py
@@ -1,2 +1,12 @@
 from tkinter import *
 
+canvas = Canvas(width=500, height=500)
+canvas.pack()
+
+floor = PhotoImage(file='src/floor.gif')
+
+for i in range(10):
+    for j in range(10):
+        canvas.create_image(50 * (j+1), 50 * (i+1), image=floor, anchor=NW)
+
+mainloop()
