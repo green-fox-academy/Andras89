72e75ffd476c7f34adb15cceea1ed9994afc9b55
Andras <palfi.andras89@gmail.com>
2018-07-23 16:44:52 +0200

    feat(RedditController): MainPage()

--- Java/GradlePractice/RedditCloneApp/src/main/java/com/greenfoxacademy/controllers/RedditController.java
+++ Java/GradlePractice/RedditCloneApp/src/main/java/com/greenfoxacademy/controllers/RedditController.java
@@ -1,7 +1,13 @@
 package com.greenfoxacademy.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RedditController {
+
+    @GetMapping("")
+    public String MainPage(){
+        return "main";
+    }
 }
