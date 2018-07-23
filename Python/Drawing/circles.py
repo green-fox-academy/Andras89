575d0058c814e24905813aec0392c5546ce77b55
Andras <palfi.andras89@gmail.com>
2018-07-23 22:49:55 +0200

    feat(circles): lots of circles

--- Python/Drawing/circles.py
+++ Python/Drawing/circles.py
@@ -5,11 +5,33 @@ draw = Pen()
 draw.pensize(1)
 draw.pencolor("blue")
 draw.speed(0)
-cicle = 84
 
-for i in range(cicle):
-    draw.circle(120)
-    draw.right(360/cicle)
+outer = 4
+pos = 8
+cicle = 16
 
+for u in range(outer):
+    draw.penup()
+    draw.forward(60)
+    draw.pendown()
+
+    for j in range(pos):
+        draw.penup()
+        draw.forward(60)
+        draw.pendown()
+
+        for i in range(cicle):
+            draw.circle(120)
+            draw.right(360/cicle)
+
+        draw.penup()
+        draw.backward(60)
+        draw.pendown()
+        draw.left(360/pos)
+
+    draw.penup()
+    draw.backward(60)
+    draw.pendown()
+    draw.left(360 / outer)
 
 tkinter.mainloop()
