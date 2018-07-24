7515bd0936882e7ac7ba32e92a431c31311f558c
Andras <palfi.andras89@gmail.com>
2018-07-24 15:43:52 +0200

    feat(wanderer_main): render map floor

--- Python/Wanderer/wanderer_main.py
+++ Python/Wanderer/wanderer_main.py
@@ -2,10 +2,27 @@ import pygame
 
 pygame.init()
 
-resolution = (600, 600)
-gameDisplay = pygame.display.set_mode(resolution)
+canvasWidth = 720
+canvasHeight = 720
+
+white = (0, 0, 0)
+
+gameDisplay = pygame.display.set_mode((canvasWidth, canvasHeight))
 pygame.display.set_caption('Wanderer the RPG Game')
 clock = pygame.time.Clock()
+
+floorImg = pygame.image.load('src/floor.gif')
+
+
+def floor(x, y):
+    gameDisplay.blit(floorImg, (x, y))
+
+
+def map():
+    for i in range(10):
+        for j in range(10):
+            floor(j * 72, i * 72)
+
 
 gameExit = False
 
@@ -17,8 +34,9 @@ while not gameExit:
 
         print(event)
 
+    gameDisplay.fill(white)
+    map()
     pygame.display.update()
     clock.tick(60)
-
 
 pygame.quit()
