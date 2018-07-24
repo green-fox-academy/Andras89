8404c5d27c4fe6e146eec2bf7010af3bd87fad57
Andras <palfi.andras89@gmail.com>
2018-07-24 23:47:05 +0200

    add(Wanderer): game_logic.py

---
+++ Python/Wanderer/game_logic.py
@@ -0,0 +1,78 @@
+from entities import GameMap, Entity, Hero
+
+
+class GameLogic:
+    def __init__(self, canvasWidth, canvasHeight, mapWidth, mapHeight):
+        self.gameMap = GameMap(canvasWidth, canvasHeight, mapWidth, mapHeight)
+
+    def populate_entities(self, inputHero):
+        self.gameMap.entityContainer = []
+        self.populate_tiles()
+        self.populate_hero(inputHero)
+
+    def populate_tiles(self):
+        mapContainer = [x.strip() for x in open('src/map/MAPS.txt', encoding='utf-8-sig')]
+
+        for i in range(int(self.gameMap.mapHeight / self.gameMap.entityHeight)):
+            for j in range(int(self.gameMap.mapWidth / self.gameMap.entityWidth)):
+                temp = Entity()
+                temp.positionWidth = self.gameMap.mapOffSetWidth + (j * self.gameMap.entityWidth)
+                temp.positionHeight = self.gameMap.mapOffSetHeight + (i * self.gameMap.entityHeight)
+
+                if mapContainer[i][j] == '1':
+                    self.gameMap.entityContainer.append(temp)
+                else:
+                    temp.canBePassed = False
+                    self.gameMap.entityContainer.append(temp)
+
+    def populate_hero(self, inputHero):
+        hero = inputHero
+        hero.positionWidth = self.gameMap.mapOffSetWidth
+        hero.positionHeight = self.gameMap.mapOffSetHeight
+        self.gameMap.entityContainer.append(hero)
+
+
+    def get_hero_index(self):
+        hero = next(x for x in self.gameMap.entityContainer if type(x) is Hero)
+        heroIndex = self.gameMap.entityContainer.index(hero)
+
+        return heroIndex
+
+    def move_hero_horizontal(self, heroMoveWidth):
+        if heroMoveWidth < 0:
+            self.gameMap.entityContainer[self.get_hero_index()].facing = 'L'
+        if heroMoveWidth > 0:
+            self.gameMap.entityContainer[self.get_hero_index()].facing = 'R'
+
+        heroPosWidth = self.gameMap.entityContainer[self.get_hero_index()].positionWidth
+        heroPosHeight = self.gameMap.entityContainer[self.get_hero_index()].positionHeight
+        selectedWidth = heroPosWidth + heroMoveWidth
+
+        descisor = next((x for x in self.gameMap.entityContainer if x.positionWidth == selectedWidth and
+                         x.positionHeight == heroPosHeight and
+                         x.canBePassed), 0)
+
+        if type(descisor) is Entity:
+            self.gameMap.entityContainer[self.get_hero_index()].positionWidth += heroMoveWidth
+
+    def move_hero_vertical(self, heroMoveHeight):
+        if heroMoveHeight < 0:
+            self.gameMap.entityContainer[self.get_hero_index()].facing = 'U'
+        if heroMoveHeight > 0:
+            self.gameMap.entityContainer[self.get_hero_index()].facing = 'D'
+
+        heroPosWidth = self.gameMap.entityContainer[self.get_hero_index()].positionWidth
+        heroPosHeight = self.gameMap.entityContainer[self.get_hero_index()].positionHeight
+        selectedHeight = heroPosHeight + heroMoveHeight
+
+        descisor = next((x for x in self.gameMap.entityContainer if x.positionHeight == selectedHeight and
+                         x.positionWidth == heroPosWidth and
+                         x.canBePassed), 0)
+
+        if type(descisor) is Entity:
+            self.gameMap.entityContainer[self.get_hero_index()].positionHeight += heroMoveHeight
+
+
+log = GameLogic(600, 600, 500, 500)
+log.populate_entities(Hero())
+log.move_hero_vertical(log.gameMap.entityHeight)
