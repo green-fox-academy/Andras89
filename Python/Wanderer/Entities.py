f22d57c0db8683c39861a2196e83528dca2e7649
Andras <palfi.andras89@gmail.com>
2018-07-24 19:36:28 +0200

    feat(entities): methods

--- Python/Wanderer/Entities.py
+++ Python/Wanderer/Entities.py
@@ -25,27 +25,46 @@ class GameMap:
         self.mapOffSetWidth = (canvasWidth - mapWidth) / 2
         self.mapOffSetHeight = (canvasHeight - mapHeight) / 2
         self.entityContainer = self.populate_entities()
-        self.hero = Character()
 
     def populate_entities(self):
-        mapContainer = [x.rstrip() for x in open('src/map/MAPS.txt')]
+        mapContainer = [x.strip() for x in open('src/map/MAPS.txt', encoding='utf-8')]
         entities = []
 
         for i in range(10):
             for j in range(10):
-                if mapContainer[i][j] == 1:
-                    temp = Entity()
-                    temp.positionWidth = self.mapOffSetWidth + (j * self.entityWidth)
-                    temp.positionHeight = self.mapOffSetHeight + (i * self.entityHeight)
+                temp = Entity()
+                temp.positionWidth = self.mapOffSetWidth + (j * self.entityWidth)
+                temp.positionHeight = self.mapOffSetHeight + (i * self.entityHeight)
+
+                if mapContainer[i][j] == '1':
                     entities.append(temp)
                 else:
-                    temp = Entity()
-                    temp.positionWidth = self.mapOffSetWidth + (j * self.entityWidth)
-                    temp.positionHeight = self.mapOffSetHeight + (i * self.entityHeight)
                     temp.canBePassed = False
                     entities.append(temp)
+        entities = self.populate_hero(entities)
 
         return entities
 
+    def populate_hero(self, entities):
+        hero = Character()
+        hero.positionWidth = self.mapOffSetWidth
+        hero.positionHeight = self.mapOffSetHeight
+        entities.append(hero)
+
+        return entities
+
+    def move_hero_horizontal(self, heroMoveWidth):
+        heroPosWidth = self.entityContainer[100].positionWidth
+        heroPosHeight = self.entityContainer[100].positionHeight
+        selectedWidth = heroPosWidth + heroMoveWidth
+
+        descisor = next((x for x in self.entityContainer if x.positionWidth == selectedWidth and
+                         x.positionHeight == heroPosHeight and
+                         x.canBePassed), 0)
+
+        if type(descisor) is Entity:
+            self.entityContainer[100].positionWidth += heroMoveWidth
+
 
 gameScape = GameMap(600, 600, 500, 500)
+gameScape.move_hero_horizontal(50)
