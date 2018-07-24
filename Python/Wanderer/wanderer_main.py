4c1125171af5b648e5de6d837116a4f21049d48b
Andras <palfi.andras89@gmail.com>
2018-07-24 15:13:29 +0200

    feat(wanderer_main): game init

--- Python/Wanderer/wanderer_main.py
+++ Python/Wanderer/wanderer_main.py
@@ -1,2 +1,8 @@
 import pygame
 
+pygame.init()
+
+resolution = (800, 800)
+gameDisplay = pygame.display.set_mode(resolution)
+pygame.display.set_caption('Wanderer the RPG Game')
+clock = pygame.time.Clock()
