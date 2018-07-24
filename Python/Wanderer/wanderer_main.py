f505b482daad0735337f1a17813d486c4bb868d8
Andras <palfi.andras89@gmail.com>
2018-07-24 16:13:35 +0200

    feat(wanderer_main): render hero

--- Python/Wanderer/wanderer_main.py
+++ Python/Wanderer/wanderer_main.py
@@ -10,6 +10,8 @@ entityWidth = mapWidth/10
 entityHeight = mapHeight/10
 mapOffSetWidth = (canvasWidth - mapWidth)/2
 mapOffSetHeight = (canvasHeight - mapHeight)/2
+heroPosWidth = mapOffSetWidth
+heroPosHeight = mapOffSetHeight
 
 white = (0, 0, 0)
 
@@ -17,14 +19,19 @@ gameDisplay = pygame.display.set_mode((canvasWidth, canvasHeight))
 pygame.display.set_caption('Wanderer the RPG Game')
 clock = pygame.time.Clock()
 
-floorImg = pygame.transform.scale((pygame.image.load('src/floor.gif')), (int(entityWidth), int(entityHeight)))
+floorImg = pygame.transform.scale(pygame.image.load('src/floor.gif'), (int(entityWidth), int(entityHeight)))
+heroImg = pygame.transform.scale(pygame.image.load('src/hero-down.gif'), (int(entityWidth), int(entityHeight)))
 
 
 def floor(x, y):
     gameDisplay.blit(floorImg, (x, y))
 
 
-def map():
+def hero(x, y):
+    gameDisplay.blit(heroImg, (x, y))
+
+
+def gameMap():
     for i in range(10):
         for j in range(10):
             floor(mapOffSetWidth+(j * entityWidth), mapOffSetHeight+(i * entityHeight))
@@ -41,7 +48,8 @@ while not gameExit:
         print(event)
 
     gameDisplay.fill(white)
-    map()
+    gameMap()
+    hero(heroPosWidth, heroPosHeight)
     pygame.display.update()
     clock.tick(60)
 
