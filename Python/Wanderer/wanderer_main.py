5e37d1265ddfe8dbddcbf2500f854ea3ebec4de8
Andras <palfi.andras89@gmail.com>
2018-07-24 15:21:57 +0200

    feat(wanderer_main): run environment

--- Python/Wanderer/wanderer_main.py
+++ Python/Wanderer/wanderer_main.py
@@ -2,7 +2,23 @@ import pygame
 
 pygame.init()
 
-resolution = (800, 800)
+resolution = (600, 600)
 gameDisplay = pygame.display.set_mode(resolution)
 pygame.display.set_caption('Wanderer the RPG Game')
 clock = pygame.time.Clock()
+
+gameExit = False
+
+while not gameExit:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+
+        print(event)
+
+    pygame.display.update()
+    clock.tick(60)
+
+
+pygame.quit()
